22ebda4453c967099beb958c3419d1a4e482c39e
brian d foy <brian.d.foy@gmail.com>
2010-12-22 07:48:52 -0600

    Add check_json.pl to check JSON format of files

---
+++ bin/check_json.pl
@@ -0,0 +1,15 @@
+use 5.010;
+
+use Data::Dumper;
+use JSON::XS;
+
+foreach my $file ( @ARGV ) {
+	say "Processing $file";
+	eval { 
+		my $hash = decode_json( do { local( @ARGV, $/ ) = $file; <> } );
+		print Dumper( $hash );
+		};
+
+	if( $@ ) { say "\terror in $file: $@" }
+	}
+	
