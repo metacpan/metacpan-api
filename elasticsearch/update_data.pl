f4ea149a4e849961762a8580953048b346d9493b
Olaf Alders <olaf@wundersolutions.com>
2011-01-04 08:47:53 -0500

    ES index updater now uses wget rather than WWW::Mechanize.

--- elasticsearch/update_data.pl
+++ elasticsearch/update_data.pl
@@ -5,45 +5,45 @@
 The index is created via cron on a different machine. Here we port it over and
 restart ES with new data.
 
-eg: perl elasticsearch/update_data.pl --file "http://www.ww5.wundercounter.com/metacpan/data.zip" --elasticsearch "/Users/olaf/Documents/developer/elasticsearch-0.13.1"
+eg:
+
+perl elasticsearch/update_data.pl --file "http://www.ww5.wundercounter.com/metacpan/data.zip" --elasticsearch "/Users/olaf/Documents/developer/elasticsearch-0.13.1"
 
 =cut
 
 use Archive::Tar::Wrapper;
 use Data::Dump qw( dump );
 use File::Copy::Recursive qw( dirmove );
+use File::stat;
 use Find::Lib '../lib';
 use Getopt::Long::Descriptive;
 use MetaCPAN;
 use Modern::Perl;
-use Path::Class::File;
-use WWW::Mechanize;
-use WWW::Mechanize::Cached;
 
-my ($opt, $usage) = describe_options(
-  'update_data.pl %o',
-  [ 'file|f=s', "the location of the data.zip" ],
-  [ 'elasticsearch|es=s', "the location of ElasticSearch" ],
-  [ 'help',       "print usage message and exit" ],
+my ( $opt, $usage ) = describe_options(
+    'update_data.pl %o',
+    [ 'file|f=s',           "the location of the archived ElasticSeach data folder" ],
+    [ 'elasticsearch|es=s', "the location of ElasticSearch" ],
+    [ 'help',               "print usage message and exit" ],
 );
 
-print($usage->text), exit if $opt->help;
+print( $usage->text ), exit if ( $opt->help || !$opt->file || !$opt->es );
 
 my $mech_class = 'WWW::Mechanize::Cached';
-my $es       = MetaCPAN->new->es;
-my $filename = '/tmp/metacpan_data.zip';
-my $file     = Path::Class::File->new( $filename );
-my $mech     = $mech_class->new;
+my $es         = MetaCPAN->new->es;
+my $filename   = '/tmp/metacpan_data.zip';
 
-$mech->get( $opt->file );
-my $fh = $file->openw();
-print $fh $mech->content;
+# refresh files more than 12 hours old
+if ( !-e $filename || (stat($filename))[9] < time() - 3600 * 12 ) {
+    my $wget = "wget -O $filename " . $opt->file;
+    `$wget`;
+}
 
 my $arch = Archive::Tar::Wrapper->new();
-$arch->read( $filename );
+$arch->read( $filename ) || die "cannot read archive";
 
 $es->shutdown;
-dirmove ( $arch->tardir, $opt->elasticsearch );
+dirmove( $arch->tardir, $opt->elasticsearch );
 
 my $start = $opt->elasticsearch . '/bin/elasticsearch';
 `$start`;
