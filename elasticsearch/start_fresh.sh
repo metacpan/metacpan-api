e5668f3981e907437f35266cdf4855cbc11f68a6
Olaf Alders <olaf@wundersolutions.com>
2011-01-05 22:44:50 -0600

    Adds a script which can be used to create and index a new instance of ES.

---
+++ elasticsearch/start_fresh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+perl delete_index.pl cpan
+perl create_index.pl cpan
+perl map_modules.pl
+perl index_authors.pl
+perl index_cpanratings.pl
+perl index_dists.pl --refresh_db
+#cd /home/cpan/elasticsearch && zip -r /home/cpan/data_snapshot.zip data
