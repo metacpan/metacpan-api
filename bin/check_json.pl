a250fcce04dab72fd5176ca4f0101b4af8e75162
Olaf Alders <olaf@wundersolutions.com>
2011-01-14 00:40:24 -0500

    PerlTidy

--- bin/check_json.pl
+++ bin/check_json.pl
@@ -4,12 +4,14 @@ use Data::Dumper;
 use JSON::XS;
 
 foreach my $file ( @ARGV ) {
-	say "Processing $file";
-	eval { 
-		my $hash = decode_json( do { local( @ARGV, $/ ) = $file; <> } );
-		print Dumper( $hash );
-		};
+    say "Processing $file";
+    eval {
+        my $hash = decode_json(
+            do { local ( @ARGV, $/ ) = $file; <> }
+        );
+        print Dumper( $hash );
+    };
 
-	if( $@ ) { say "\terror in $file: $@" }
-	}
-	
+    if ( $@ ) { say "\terror in $file: $@" }
+}
+
