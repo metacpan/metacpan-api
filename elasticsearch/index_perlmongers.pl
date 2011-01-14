574f1b62cfb0c967b303a5956c34a0034b76bf84
Olaf Alders <olaf@wundersolutions.com>
2011-01-14 00:38:48 -0500

    Adds functionality for indexing all listed PerlMongers groups.  Still broken, though.

---
+++ elasticsearch/index_perlmongers.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+=head1 SYNOPSIS
+
+Loads PerlMonger groups into db. 
+
+    perl index_perlmongers.pl
+
+=cut
+
+use Modern::Perl;
+use Data::Dump qw( dump );
+use Find::Lib '../lib';
+use MetaCPAN::PerlMongers;
+
+my $author = MetaCPAN::PerlMongers->new;
+my $result = $author->index_perlmongers;
+say dump( $result );
+
+$author->es->refresh_index( index => 'cpan' );
--- lib/MetaCPAN.pm
+++ lib/MetaCPAN.pm
@@ -314,26 +314,87 @@ sub map_cpanratings {
 
 }
 
-sub put_mappings {
-    
+sub map_perlmongers {
+
     my $self = shift;
+    return $self->es->put_mapping(
+        index      => ['cpan'],
+        type       => 'perlmongers',
+        properties => {
+            city      => { type       => "string" },
+            continent => { type       => "string" },
+            email     => { properties => { type => { type => "string" } } },
+            inception_date =>
+                { format => "dateOptionalTime", type => "date" },
+            latitude => { type => "object" },
+            location => {
+                properties => {
+                    city      => { type => "string" },
+                    continent => { type => "string" },
+                    country   => { type => "string" },
+                    latitude  => { type => "string" },
+                    longitude => { type => "string" },
+                    region    => { type => "object" },
+                    state     => { type => "string" },
+                },
+            },
+            longitude    => { type => "object" },
+            mailing_list => {
+                properties => {
+                    email => {
+                        properties => {
+                            domain => { type => "string" },
+                            type   => { type => "string" },
+                            user   => { type => "string" },
+                        },
+                    },
+                    name => { type => "string" },
+                },
+            },
+            name   => { type => "string" },
+            pm_id  => { type => "string" },
+            region => { type => "string" },
+            state  => { type => "object" },
+            status => { type => "string" },
+            tsar   => {
+                properties => {
+                    email => {
+                        properties => {
+                            domain => { type => "string" },
+                            type   => { type => "string" },
+                            user   => { type => "string" },
+                        },
+                    },
+                    name => { type => "string" },
+                },
+            },
+            web => { type => "string" },
+        },
+
+    );
+
+}
+
+sub put_mappings {
+
+    my $self  = shift;
     my @types = qw( author cpanratings dist module pod );
-    
+
     foreach my $type ( @types ) {
         $self->es->delete_mapping(
             index => ['cpan'],
             type  => $type,
-        );        
+        );
     }
-    
+
     $self->map_author;
     $self->map_cpanratings;
     $self->map_dist;
     $self->map_module;
     $self->map_pod;
-    
+
     return;
-    
+
 }
 
 1;
