1b076d8c796d67e83a51e9e8008249b6f1ddc6a2
Olaf Alders <olaf@wundersolutions.com>
2011-01-14 00:37:54 -0500

    Adds starting point for including CPANTS results for dists.

---
+++ elasticsearch/cpants.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use Data::Dump qw( dump );
+use JSON::Any;
+use Modern::Perl;
+use WWW::Mechanize::Cached;
+
+my $j = JSON::Any->new;
+
+my $mech = WWW::Mechanize::Cached->new( autocheck => 0 );
+
+$mech->get("http://www.cpantesters.org/distro/P/Plack-Middleware-HTMLify.json");
+my $reports = $j->decode( $mech->content );
+
+my %results = ( );
+foreach my $test ( @{$reports} ) {
+    next if $test->{distversion} ne 'Plack-Middleware-HTMLify-0.1.1';
+    ++$results{ $test->{state} };
+}
+
+say dump( \%results );
