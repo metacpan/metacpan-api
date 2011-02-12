c711f9ef792e5a0075f03e137962e189375eaf8a
Moritz Onken <onken@netcubed.de>
2011-02-12 23:40:50 +0100

    ElasticSearch::Document, will eventually become its own dist

---
+++ lib/ElasticSearch/Document.pm
@@ -0,0 +1,63 @@
+package ElasticSearch::Document;
+
+use strict;
+use warnings;
+
+use Moose 1.15 ();
+use Moose::Exporter;
+use ElasticSearch::Document::Trait::Class;
+use ElasticSearch::Document::Trait::Attribute;
+use JSON::XS;
+use Digest::SHA1;
+use List::MoreUtils ();
+use Carp;
+
+Moose::Exporter->setup_import_methods(
+                    as_is           => [qw(_build_es_id index _index)],
+                    class_metaroles => {
+                        class     => ['ElasticSearch::Document::Trait::Class'],
+                        attribute => [
+                            'ElasticSearch::Document::Trait::Attribute',
+                            'MooseX::Attribute::Deflator::Meta::Role::Attribute'
+                        ]
+                    }, );
+
+
+my @stat = qw(dev ino mode nlink uid gid rdev size atime mtime ctime blksize blocks);
+use MooseX::Attribute::Deflator;
+deflate 'Bool',       via { \$_ };
+deflate 'File::stat', via { return { List::MoreUtils::mesh(@stat, @$_) } };
+deflate 'ScalarRef',  via { $$_ };
+deflate 'ArrayRef',   via { encode_json($_) };
+deflate 'DateTime',   via { $_->iso8601 };
+no MooseX::Attribute::Deflator;
+
+sub index {
+    my ( $self, $es ) = @_;
+    my $id = $self->meta->get_id_attribute;
+    return $es->index( $self->_index );
+}
+
+sub _index {
+    my ($self) = @_;
+    my $id = $self->meta->get_id_attribute;
+
+    return ( index => 'cpan',
+             type  => $self->meta->short_name,
+             $id ? ( id => $id->get_value($self) ) : (),
+             data => $self->meta->get_data($self), );
+}
+
+sub _build_es_id {
+    my $self = shift;
+    my $id   = $self->meta->get_id_attribute;
+    carp "Need an arrayref of fields for the id, not " . $id->id
+      unless ( ref $id->id eq 'ARRAY' );
+    my @fields = map { $self->meta->get_attribute($_) } @{ $id->id };
+    my $digest = join( "\0", map { $_->get_value($self) } @fields );
+    $digest = Digest::SHA1::sha1_base64($digest);
+    $digest =~ tr/[+\/]/-_/;
+    return $digest;
+}
+
+1;
---
+++ lib/ElasticSearch/Document/Trait/Attribute.pm
@@ -0,0 +1,56 @@
+package ElasticSearch::Document::Trait::Attribute;
+use Moose::Role;
+
+has property => ( is => 'ro', isa => 'Bool', default => 1 );
+
+has id => ( is => 'ro', isa => 'Bool|ArrayRef', default => 0 );
+has index => ( is => 'ro', lazy_build => 1 );
+has boost => ( is => 'ro', isa        => 'Num', default => 1.0 );
+has store => ( is => 'ro', isa        => 'Str', default => 'yes' );
+has type  => ( is => 'ro', isa        => 'Str', lazy_build => 1 );
+
+
+
+sub _build_type {
+    my $self = shift;
+    my $tc = $self->type_constraint ? $self->type_constraint->name : 'Str';
+    my %map = ( Int      => 'integer',
+                Str      => 'string',
+                DateTime => 'date',
+                Num      => 'float',
+                Bool     => 'boolean',
+                Undef    => 'null',
+                HashRef  => 'object',
+                ArrayRef => 'string' );
+    return $map{$tc} || 'string';
+}
+
+sub _build_index {
+    my $self = shift;
+    return $self->type eq 'string' ? 'not_analyzed' : undef;
+}
+
+sub is_property { shift->property }
+
+sub es_properties {
+    my $self = shift;
+    my $props = { store => $self->store,
+                  $self->index ? ( index => $self->index ) : (),
+                  boost => $self->boost,
+                  type  => $self->type };
+    if ( $self->has_type_constraint ) {
+        $props->{dynamic} = \0
+          if ( $self->type_constraint->name =~ /Ref/ );
+    }
+    return $props;
+}
+
+before _process_options => sub {
+    my ( $self, $name, $options ) = @_;
+    $options->{required} = 1    unless ( exists $options->{required} );
+    $options->{is}       = 'ro' unless ( exists $options->{is} );
+    %$options = ( builder => '_build_es_id', lazy => 1, %$options )
+      if ( $options->{id} && ref $options->{id} eq 'ARRAY' );
+};
+
+1;
