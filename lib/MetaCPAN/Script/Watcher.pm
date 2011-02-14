de244fa95dd1b86a30d147c05d19a120d4e75918
Moritz Onken <onken@netcubed.de>
2011-02-14 20:05:02 +0100

    Watcher script that indexes releases when they are uploaded

---
+++ lib/MetaCPAN/Script/Watcher.pm
@@ -0,0 +1,53 @@
+package MetaCPAN::Script::Watcher;
+
+use Moose;
+with 'MooseX::Getopt';
+with 'MetaCPAN::Role::Common';
+
+use feature qw(say);
+use AnyEvent::FriendFeed::Realtime;
+use AnyEvent::Run;
+
+my $fails = 0;
+sub run {
+    my $self = shift;
+    say "Reconnecting after $fails fails" if($fails);
+    my($user, $remote_key, $request) = @ARGV;
+    my $done = AnyEvent->condvar;
+
+    binmode STDOUT, ":utf8";
+    my %handles;
+    my $client = AnyEvent::FriendFeed::Realtime->new(
+        request    => "/feed/cpan",
+        on_entry   => sub {
+            my $entry = shift;
+            $entry->{body} =~ /href="(.*?)"/;
+            my $file = $1;
+            return unless( $file );
+            $handles{$file} = AnyEvent::Run->new(
+                class => 'MetaCPAN::Script::Release',
+                args => [$file],
+                on_read => sub { },
+                on_eof => sub { },
+                on_error  => sub {
+                    my ($handle, $fatal, $msg) = @_;
+                    my $arg = $handle->{args}->[0];
+                    say "Indexing $arg done";
+                    say $handle->rbuf;
+                }
+            );
+        },
+        on_error   => sub {
+            $done->send;
+        },
+    );
+    say "Up and running. Watching for updates on http://friendfeed.com/cpan ..."
+        unless($fails);
+    $done->recv;
+    $fails++;
+    $self->run if($fails < 5);
+    say "Giving up after $fails fails";
+    
+}
+
+1;
