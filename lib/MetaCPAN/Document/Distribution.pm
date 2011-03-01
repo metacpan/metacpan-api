ec8f3428bd7c027d108969a12c82a2fd8f5736d7
Moritz Onken <onken@netcubed.de>
2011-03-02 00:56:37 +0100

    analyze name

--- lib/MetaCPAN/Document/Distribution.pm
+++ lib/MetaCPAN/Document/Distribution.pm
@@ -2,7 +2,7 @@ package MetaCPAN::Document::Distribution;
 use Moose;
 use ElasticSearch::Document;
 
-has name    => ( id       => 1 );
+has name    => ( id       => 1, index => 'analyzed' );
 has ratings => ( isa      => 'Int', default => 0 );
 has rating  => ( required => 0, isa => 'Num' );
 has [qw(pass fail na unknown)] => ( isa => 'Int', default => 0 );
