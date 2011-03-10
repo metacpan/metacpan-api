c6a7e63705e9ec8a1fdbf46163110580f5a3f06f
Moritz Onken <onken@netcubed.de>
2011-03-10 22:52:17 +0100

    log to file in interactive mode as well

--- etc/metacpan_interactive.pl
+++ etc/metacpan_interactive.pl
@@ -1,7 +1,12 @@
+use FindBin;
 {
     level => 'debug',
     logger => [{
         class => 'Log::Log4perl::Appender::ScreenColoredLevels',
         stdout => 0,
+    }, {
+        class => 'Log::Log4perl::Appender::File',
+        filename => $FindBin::RealBin . '/../var/log/metacpan.log',
+        syswrite => 1,
     }]
 }
