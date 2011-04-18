e7be714f3d46420d63b37d6cf707d40bb731a4aa
Moritz Onken <onken@netcubed.de>
2011-04-18 09:20:04 +0200

    updated author mapping, refs #80

--- lib/MetaCPAN/Document/Author.pm
+++ lib/MetaCPAN/Document/Author.pm
@@ -4,14 +4,26 @@ use ElasticSearchX::Model::Document;
 use Gravatar::URL ();
 use MetaCPAN::Util;
 
+use MetaCPAN::Types qw(:all);
+use MooseX::Types::Structured qw(Dict Tuple Optional);
+use MooseX::Types::Moose qw/Int Num Str ArrayRef HashRef Undef/;
+use ElasticSearchX::Model::Document::Types qw(:all);
+
 # TODO: replace censored emailadresse with cpan emailadress
 
 has name => ( index => 'analyzed' );
-has email => ( );
+has email => ( isa => ArrayRef, coerce => 1 );
 has 'pauseid' => ( id         => 1 );
 has 'author'  => ( lazy_build => 1 );
 has 'dir'     => ( lazy_build => 1 );
 has 'gravatar_url' => ( lazy_build => 1 );
+has profile => ( isa => Dict[ name => Str, id => Str ], required => 0 );
+has blog => ( isa => Dict[ url => Str, feed => Str ], required => 0 );
+has perlmongers => ( isa => Dict[ url => Str, name => Str ], required => 0 );
+has donation => ( isa => Dict[ name => Str, id => Str ], required => 0 );
+has [qw(email website city region country)] => ( required => 0 );
+has location => ( isa => Location, coerce => 1, required => 0 );
+has extra => ( isa => Extra, required => 0 );
 
 sub _build_dir {
     my $pauseid = ref $_[0] ? shift->pauseid : shift;
@@ -19,17 +31,11 @@ sub _build_dir {
 }
 
 sub _build_gravatar_url {
-    Gravatar::URL::gravatar_url( email => shift->email );
+    my $self = shift;
+    my $email = ref $self->email ? $self->email->[0] : $self->email;
+    Gravatar::URL::gravatar_url( email => $email );
 }
 
 sub _build_author { shift->name }
-has [qw(accepts_donations amazon_author_profile blog_feed blog_url 
-       books cats city country delicious_username dogs 
-       facebook_public_profile github_username irc_nick 
-       linkedin_public_profile openid oreilly_author_profile 
-       paypal_address perlmongers perlmongers_url perlmonks_username 
-       region slideshare_url slideshare_username 
-       stackoverflow_public_profile twitter_username website 
-       youtube_channel_url)]
-  => ( required => 0 );
+
 __PACKAGE__->meta->make_immutable;
