9450ee54091c6fa1a8ac9f4cb15847ba3a810d91
Moritz Onken <onken@netcubed.de>
2011-04-29 12:34:43 +0200

    added index alias

--- lib/MetaCPAN/Model.pm
+++ lib/MetaCPAN/Model.pm
@@ -6,7 +6,7 @@ analyzer lowercase => ( tokenizer => 'keyword', filter => 'lowercase' );
 analyzer fulltext => ( type => 'snowball', language => 'English' );
 analyzer camelcase => ( type => 'pattern', pattern => "(\\W+)|(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z])" );
 
-index cpan => ( namespace => 'MetaCPAN::Document' );
+index cpan => ( namespace => 'MetaCPAN::Document', alias_for => 'cpan_v3' );
 
 index user => ( namespace => 'MetaCPAN::Model::User' );
 
