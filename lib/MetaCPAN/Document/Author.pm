2a6098d68d9ef4f5c71aa22335bc41855b1c01a4
Moritz Onken <onken@netcubed.de>
2011-05-28 12:04:24 +0200

    removed author property

--- lib/MetaCPAN/Document/Author.pm
+++ lib/MetaCPAN/Document/Author.pm
@@ -82,19 +82,18 @@ analyzed JSON string.
 
 =cut
 
-has name => ( index => 'analyzed' );
-has email => ( isa => ArrayRef, coerce => 1 );
-has 'pauseid' => ( id         => 1 );
-has 'author'  => ( lazy_build => 1 );
-has 'dir'     => ( lazy_build => 1 );
-has 'gravatar_url' => ( lazy_build => 1 );
-has profile => ( isa => Dict[ name => Str, id => Str ], required => 0 );
-has blog => ( isa => Dict[ url => Str, feed => Str ], required => 0 );
-has perlmongers => ( isa => Dict[ url => Str, name => Str ], required => 0 );
-has donation => ( isa => Dict[ name => Str, id => Str ], required => 0 );
+has name         => ( index      => 'analyzed' );
+has email        => ( isa        => ArrayRef, coerce => 1 );
+has pauseid      => ( id         => 1 );
+has dir          => ( lazy_build => 1 );
+has gravatar_url => ( lazy_build => 1 );
+has profile => ( isa => Dict [ name => Str, id => Str ], required => 0 );
+has blog        => ( isa => Dict [ url  => Str, feed => Str ], required => 0 );
+has perlmongers => ( isa => Dict [ url  => Str, name => Str ], required => 0 );
+has donation    => ( isa => Dict [ name => Str, id   => Str ], required => 0 );
 has [qw(website city region country)] => ( required => 0 );
-has location => ( isa => Location, coerce => 1, required => 0 );
-has extra => ( isa => Extra, required => 0, index => 'analyzed' );
+has location => ( isa => Location, coerce   => 1, required => 0 );
+has extra    => ( isa => Extra,    required => 0, index    => 'analyzed' );
 
 sub _build_dir {
     my $pauseid = ref $_[0] ? shift->pauseid : shift;
@@ -106,7 +105,5 @@ sub _build_gravatar_url {
     my $email = ref $self->email ? $self->email->[0] : $self->email;
     Gravatar::URL::gravatar_url( email => $email );
 }
-
-sub _build_author { shift->name }
 
 __PACKAGE__->meta->make_immutable;
