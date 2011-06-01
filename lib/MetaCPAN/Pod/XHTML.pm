3ff77efe06ae7c115e0ad5de77816b0f2bca4d23
Moritz Onken <onken@netcubed.de>
2011-06-01 21:49:06 +0200

    fix links in html pod

--- lib/MetaCPAN/Pod/XHTML.pm
+++ lib/MetaCPAN/Pod/XHTML.pm
@@ -3,49 +3,17 @@ package MetaCPAN::Pod::XHTML;
 use Moose;
 extends 'Pod::Simple::XHTML';
 
-sub start_L {
-    my ( $self, $flags ) = @_;
-    my ( $type, $to, $section ) = @{$flags}{ 'type', 'to', 'section' };
-
-    my $url
-        = $type eq 'url' ? $to
-        : $type eq 'pod' ? $self->resolve_pod_page_link( $to, $section )
-        : $type eq 'man' ? $self->resolve_man_page_link( $to, $section )
-        :                  undef;
-    $url ||= '';
-    my $pound = '#';
-    my $class
-        = ( $type eq 'pod' && ($url !~ m{$pound}) ) ? ' class="moduleLink"' : '';
-
-    $self->{'scratch'} .= qq[<a href="$url"$class>];
-}
-
-sub start_Verbatim {
-
-}
-
-sub end_Verbatim {
-
-    $_[0]{'scratch'} = '<pre>' . $_[0]{'scratch'} . '</pre>';
-    $_[0]->emit;
-
+sub perldoc_url_prefix {
+    'http://metacpan.org/module/'
 }
 
 1;
 
 =pod
 
-=head2 start_L
+=head2 perldoc_url_prefix
 
-Add the "moduleLink" class to any hrefs which link directly to module docs.
-
-=head2 start_Verbatim
-
-Override default behaviour by doing nothing.
-
-=head2 end_Verbatim
-
-Wrap code snippets in <pre> tags for easier syntax highlighting.
+Set perldoc domain to C<metacpan.org>.
 
 =cut
 
