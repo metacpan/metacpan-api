36b2ae6d4a378bc5df6a9a35af0ab69646c7a177
Moritz Onken <onken@netcubed.de>
2011-06-17 21:16:10 +0200

    fixed authorized indexer

--- lib/MetaCPAN/Script/Authorized.pm
+++ lib/MetaCPAN/Script/Authorized.pm
@@ -15,38 +15,57 @@ sub run {
     $self->index->refresh;
     log_info { "Dry run: updates will not be written to ES" }
     if ( $self->dry_run );
-    my @unauthorized;
+    my @authorized;
     my ( $authors, $perl ) = $self->parse_perms;
     my $scroll = $self->scroll;
     log_info { $scroll->total . " modules found" };
 
     while ( my $file = $scroll->next ) {
         my $data = $file->{fields};
-        next if ( $data->{distribution} eq 'perl' );
         my @modules =
           map  { $_->{name} }
           grep { $_->{indexed} } @{ $data->{'_source.module'} };
         foreach my $module (@modules) {
-            next
-              if ( $authors->{$module}
-                && grep { $_ eq $data->{author} } @{ $authors->{$module} } );
-
-            log_debug {
-"unauthorized module $module in $data->{release} by $data->{author}";
-            };
-            push( @unauthorized, { file => $file->{_id}, module => $module } );
-            if ( @unauthorized == 100 ) {
-                $self->bulk_update(@unauthorized);
-                @unauthorized = ();
+            if (
+                $data->{distribution} eq 'perl'
+                || ( $authors->{$module}
+                    && grep { $_ eq $data->{author} } @{ $authors->{$module} } )
+              )
+            {
+                push(
+                    @authorized,
+                    {
+                        file       => $file->{_id},
+                        module     => $module,
+                        authorized => \1
+                    }
+                );
             }
-
+            else {
+                log_debug {
+"unauthorized module $module in $data->{release} by $data->{author}";
+                };
+                push(
+                    @authorized,
+                    {
+                        file       => $file->{_id},
+                        module     => $module,
+                        authorized => \0
+                    }
+                );
+            }
+        }
+        if ( @authorized == 100 ) {
+            $self->bulk_update(@authorized);
+            @authorized = ();
         }
     }
-    $self->bulk_update(@unauthorized) if (@unauthorized);    # update the rest
+    $self->bulk_update(@authorized) if (@authorized);    # update the rest
+    $self->index->refresh;
 }
 
 sub bulk_update {
-    my ( $self, @unauthorized ) = @_;
+    my ( $self, @authorized ) = @_;
     if ( $self->dry_run ) {
         log_info { "dry run, not updating" };
         return;
@@ -56,14 +75,14 @@ sub bulk_update {
     my $results = $es->search(
         index => $self->index->name,
         type  => 'file',
-        size  => scalar @unauthorized,
+        size  => scalar @authorized,
         query => {
             filtered => {
                 query  => { match_all => {} },
                 filter => {
                     or => [
                         map { { term => { 'file.id' => $_->{file} } } }
-                          @unauthorized
+                          @authorized
                     ]
                 }
             }
@@ -72,19 +91,20 @@ sub bulk_update {
     my %files =
       map { $_->{_source}->{id} => $_->{_source} }
       @{ $results->{hits}->{hits} };
-    foreach my $item (@unauthorized) {
+    foreach my $item (@authorized) {
         my $file = $files{ $item->{file} };
-        $file->{authorized} = \0
+        $file->{authorized} = $item->{authorized}
           if ( $file->{documentation}
             && $file->{documentation} eq $item->{module} );
-        map { $_->{authorized} = \0 }
+        map { $_->{authorized} = $item->{authorized} }
           grep { $_->{name} eq $item->{module} } @{ $file->{module} };
         push(
             @bulk,
             {
-                create => {
+                index => {
                     index => $self->index->name,
                     type  => 'file',
+                    id    => $file->{id},
                     data  => $file
                 }
             }
@@ -107,12 +127,13 @@ sub scroll {
                             {
                                 not => {
                                     filter => {
-                                        term =>
-                                          { 'file.module.authorized' => \0 }
+                                        exists =>
+                                          { field => 'file.module.authorized' }
                                     }
                                 }
                             },
-                            { exists => { field => 'file.module.name' } }
+                            { term => { 'file.module.indexed' => \1 } },
+                            { exists => { field => 'file.module.name' } },
                         ]
                     }
                 }
