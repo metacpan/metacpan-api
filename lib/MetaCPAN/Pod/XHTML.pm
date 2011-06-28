bca66eba51f7e16d0982c0b2ba78f65aefae7e1b
Moritz Onken <onken@netcubed.de>
2011-06-28 08:07:02 +0200

    set anchor in =items (fixes #25)

--- lib/MetaCPAN/Pod/XHTML.pm
+++ lib/MetaCPAN/Pod/XHTML.pm
@@ -7,6 +7,24 @@ sub perldoc_url_prefix {
     'http://beta.metacpan.org/module/'
 }
 
+sub end_item_text   {
+    # idify =item content, reset 'scratch'
+    my $id = $_[0]->idify($_[0]{'scratch'});
+    my $text = $_[0]{scratch};
+    $_[0]{'scratch'} = '';
+
+    # construct whole element here because we need the
+    # contents of the =item to idify it
+    if ($_[0]{'in_dd'}[ $_[0]{'dl_level'} ]) {
+        $_[0]{'scratch'} = "</dd>\n";
+        $_[0]{'in_dd'}[ $_[0]{'dl_level'} ] = 0;
+    }
+
+    $_[0]{'scratch'} .= qq{<dt id="$id">$text</dt>\n<dd>};
+    $_[0]{'in_dd'}[ $_[0]{'dl_level'} ] = 1;
+    $_[0]->emit;
+}
+
 1;
 
 =pod
