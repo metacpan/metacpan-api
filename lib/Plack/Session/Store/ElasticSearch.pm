c7fe448f08dd0b4bdd35dacb5cc501ef417567fe
Moritz Onken <onken@netcubed.de>
2011-07-09 20:17:23 +0200

    simplified Session::Store::ES

--- lib/Plack/Session/Store/ElasticSearch.pm
+++ lib/Plack/Session/Store/ElasticSearch.pm
@@ -8,73 +8,49 @@ use Plack::Util::Accessor qw(es index type property);
 
 sub new {
     my ( $class, %params ) = @_;
-    bless { index    => 'user',
-            type     => 'session',
-            property => 'id',
-            %params
+    bless {
+        index    => 'user',
+        type     => 'session',
+        property => 'id',
+        %params
     } => $class;
 }
 
 sub fetch {
     my ( $self, $session_id ) = @_;
-    $self->find($session_id)->{_source} || {};
+    return undef unless ($session_id);
+    my $data = eval {
+        $self->es->get(
+            index => $self->index,
+            type  => $self->type,
+            id    => $session_id,
+            fields => ['_parent', '_source']
+        );
+    } || return undef;
+    $data->{_parent} = delete $data->{fields}->{_parent};
+    return $data;
 }
 
 sub store {
     my ( $self, $session_id, $session ) = @_;
-    my $old = $self->find($session_id);
-    my $sessions = $old->{ $self->property } || [];
-    $sessions = [$sessions] unless ( ref $sessions eq 'ARRAY' );
-    push( @$sessions, $session_id );
-    @$sessions = List::MoreUtils::uniq @$sessions;
-    $self->put( $old->{_id},
-                  {  %{ $old->{_source} || {} },
-                     %{ $session || {} },
-                     $self->property => $sessions
-                  } );
+    $self->es->index(
+        index => $self->index,
+        type  => $self->type,
+        id    => $session_id || undef,
+        parent => $session->{_parent} || "",
+        data => keys %$session ? $session->{_source} : { $self->type => {} },
+        refresh => 1,
+    );
 }
 
 sub remove {
     my ( $self, $session_id ) = @_;
-    my $session = $self->find($session_id);
-    my $sessions = $session->{ $self->property } || [];
-    $sessions = [$sessions] unless ( ref $sessions eq 'ARRAY' );
-    @$sessions = [ grep { $_ ne $session_id } @$sessions ];
-    $self->put( $session->{_id},
-                  {  %{ $session->{_source} },
-                     %$session,
-                     $self->property => $sessions
-                  } );
-}
-
-sub put {
-    my ( $self, $id, $data ) = @_;
-    $self->es->index( index => $self->index,
-                      type  => $self->type,
-                      id => $id || undef,
-                      data    => $data,
-                      refresh => 1, );
-}
-
-sub fqfn {
-    my $self = shift;
-    return join( '.', $self->type, $self->property );
-}
-
-sub find {
-    my ( $self, $session_id ) = @_;
-    my $res = $self->es->search(
-              index => $self->index,
-              type  => $self->type,
-              query  => { match_all => {} },
-              filter => {
-                  term => {
-                      $self->fqfn =>
-                        $session_id
-                  }
-              },
-              size => 1, );
-    return $res->{hits}->{hits}->[0] || {};
+    $self->es->delete(
+        index   => $self->index,
+        type    => $self->type,
+        id      => $session_id,
+        refresh => 1,
+    );
 }
 
 1;
