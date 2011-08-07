ea6c82dc94b102f9927d5ae0234146cda529371f
Moritz Onken <onken@netcubed.de>
2011-08-07 10:23:28 +0200

    bulk index, sort by version_numified (was date)

--- lib/MetaCPAN/Script/Latest.pm
+++ lib/MetaCPAN/Script/Latest.pm
@@ -13,12 +13,11 @@ has distribution => ( is => 'ro', isa => 'Str' );
 sub run {
     my $self = shift;
     my $es   = $self->es;
-    log_info { "Dry run: updates will not be written to ES" }
+    log_info {"Dry run: updates will not be written to ES"}
     if ( $self->dry_run );
     $self->index->refresh;
     my $scroll = $es->scrolled_search(
-        {
-            index => $self->index->name,
+        {   index => $self->index->name,
             type  => 'release',
             query => {
                 filtered => {
@@ -26,14 +25,13 @@ sub run {
                     filter => {
                         and => [
                             $self->distribution
-                            ? {
-                                term => { distribution => $self->distribution }
-                              }
+                            ? { term =>
+                                    { distribution => $self->distribution }
+                                }
                             : (),
-                            {
-                                not => {
+                            {   not => {
                                     filter =>
-                                      { term => { status => 'backpan' } }
+                                        { term => { status => 'backpan' } }
                                 }
                             }
                         ]
@@ -44,39 +42,44 @@ sub run {
             size   => 1000,
             sort   => [
                 'distribution',
-                { maturity => { reverse => \1 } },
-                { date     => { reverse => \1 } }
+                { maturity         => { reverse => \1 } },
+                { version_numified => { reverse => \1 } }
             ],
         }
     );
 
     my $dist = '';
-    while ( my $row = $scroll->next(1) ) {
+    while ( my $row = $scroll->next ) {
         my $source = $row->{_source};
         if ( $dist ne $source->{distribution} ) {
             $dist = $source->{distribution};
             next if ( $source->{status} eq 'latest' );
-            log_info { "Upgrading $source->{name} to latest" };
+            log_info {"Upgrading $source->{name} to latest"};
 
-            log_debug { "Upgrading files" };
+            log_debug {"Upgrading files"};
             $self->reindex( $source, 'latest' );
 
             next if ( $self->dry_run );
-            $es->index( index => $self->index->name,
-                        type  => 'release',
-                        id    => $row->{_id},
-                        data  => { %$source, status => 'latest' } );
-        } elsif ( $source->{status} eq 'latest' ) {
-            log_info { "Downgrading $source->{name} to cpan" };
+            $es->index(
+                index => $self->index->name,
+                type  => 'release',
+                id    => $row->{_id},
+                data  => { %$source, status => 'latest' }
+            );
+        }
+        elsif ( $source->{status} eq 'latest' ) {
+            log_info {"Downgrading $source->{name} to cpan"};
 
-            log_debug { "Downgrading files" };
+            log_debug {"Downgrading files"};
             $self->reindex( $source, 'cpan' );
 
             next if ( $self->dry_run );
-            $es->index( index => $self->index->name,
-                        type  => 'release',
-                        id    => $row->{_id},
-                        data  => { %$source , status => 'cpan' } );
+            $es->index(
+                index => $self->index->name,
+                type  => 'release',
+                id    => $row->{_id},
+                data  => { %$source, status => 'cpan' }
+            );
 
         }
     }
@@ -85,35 +88,53 @@ sub run {
 
 sub reindex {
     my ( $self, $source, $status ) = @_;
-    my $es = $self->es;
+    my $es     = $self->es;
     my $scroll = $es->scrolled_search(
-        {  index       => $self->index->name,
-           type        => 'file',
-           scroll      => '1h',
-           size        => 1000,
-           search_type => 'scan',
-           fields => ['_parent', '_source'],
-           query => { filtered => { query       => { match_all => {} },
-           filter      => {
-                       and => [
-                           { term => { 'file.release' => $source->{name} } },
-                           { term => { 'file.author'  => $source->{author} } } ]
-           } } } } );
+        {   index       => $self->index->name,
+            type        => 'file',
+            scroll      => '1h',
+            size        => 1000,
+            search_type => 'scan',
+            fields      => [ '_parent', '_source' ],
+            query       => {
+                filtered => {
+                    query  => { match_all => {} },
+                    filter => {
+                        and => [
+                            { term => { 'file.release' => $source->{name} } },
+                            {   term => { 'file.author' => $source->{author} }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    );
 
-    while ( my $row = $scroll->next(1) ) {
+    my @bulk;
+    while ( my $row = $scroll->next ) {
         my $source = $row->{_source};
         log_debug {
             $status eq 'latest' ? "Upgrading " : "Downgrading ",
-              "file ", $source->{name} || '';
+                "file ", $source->{name} || '';
         };
-        $es->index( index => $self->index->name,
-                    type  => 'file',
-                    id    => $row->{_id},
+        push(
+            @bulk,
+            {   index => {
+                    index  => $self->index->name,
+                    type   => 'file',
+                    id     => $row->{_id},
                     parent => $row->{fields}->{_parent} || "",
-                    data  => { %$source, status => $status }
+                    data   => { %$source, status => $status }
+                }
+            }
         ) unless ( $self->dry_run );
-    }
-
+        if(@bulk > 100) {
+            $self->es->bulk(\@bulk);
+            @bulk = ();
+        }
+    }    
+    $self->es->bulk(\@bulk) if(@bulk);
 }
 
 __PACKAGE__->meta->make_immutable;
