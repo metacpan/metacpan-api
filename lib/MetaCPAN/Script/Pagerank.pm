6858661dbf84c9ebd42f834d5987ce773f1c8b39
Moritz Onken <onken@netcubed.de>
2011-08-18 17:48:19 +0100

    fixed pagerank script

--- lib/MetaCPAN/Script/Pagerank.pm
+++ lib/MetaCPAN/Script/Pagerank.pm
@@ -12,7 +12,8 @@ sub run {
     my $pr   = Graph::Centrality::Pagerank->new();
     my @edges;
     my $modules = $self->get_recent_modules;
-    my $scroll  = $es->scrolled_search(
+    log_info {"Loading dependencies ..."};
+    my $scroll = $es->scrolled_search(
         index => $self->index->name,
         type  => 'release',
         query => {
@@ -31,26 +32,32 @@ sub run {
         scroll => '5m',
         size   => 1000,
     );
-    die $scroll->total;
+    log_info { $scroll->total, " recent releases found with dependencies" };
 
+    my $i = 0;
     while ( my $release = $scroll->next ) {
-        foreach my $dep ( @{ $_->{_source}->{dependency} || [] } ) {
+        foreach my $dep ( @{ $release->{_source}->{dependency} || [] } ) {
             next if ( $dep->{phase} ne 'runtime' );
-            my $dist = $modules->{ $dep->{name} };
+            my $dist = $modules->{ $dep->{module} };
             next unless ($dist);
+            $i++;
             push( @edges, [ $release->{_source}->{name}, $dist ] );
         }
     }
+    log_info {
+        "Calculating PageRankg with taking $i dependencies into account";
+    };
     my $res = $pr->getPagerankOfNodes( listOfEdges => \@edges );
     my @sort = sort { $res->{$b} <=> $res->{$a} } keys %$res;
-    for ( 1 .. 10 ) {
+    for ( 1 .. 500 ) {
         my $mod = shift @sort;
         print $mod, " ", $res->{$mod}, $/;
     }
 }
 
 sub get_recent_modules {
-    my $self   = shift;
+    my $self = shift;
+    log_info {"Mapping modules to releases ..."};
     my $scroll = $self->es->scrolled_search(
         index => $self->index->name,
         type  => 'file',
@@ -59,23 +66,34 @@ sub get_recent_modules {
                 query  => { match_all => {} },
                 filter => {
                     and => [
-                        { term => { 'file.status'         => 'latest' } },
-                        { term => { 'file.module.indexed' => \1 } },
+                        { term => { 'file.status'            => 'latest' } },
+                        { term => { 'file.module.indexed'    => \1 } },
+                        { term => { 'file.module.authorized' => \1 } },
                     ]
                 }
             }
         },
         size   => 1000,
-        fields => [qw(distribution file.module.name)],
+        fields => [
+            qw(release distribution file.module.authorized file.module.indexed file.module.name)
+        ],
         scroll => '1m',
     );
-    warn $scroll->total;
+    log_info { $scroll->total, " modules found" };
     my $result;
     while ( my $file = $scroll->next ) {
-        my $modules = $file->{fields}->{'module.name'};
-        $modules = [$modules] unless ( ref $modules );
-        foreach my $module (@$modules) {
-            $result->{$module} = $file->{fields}->{release};
+        next if ( $file->{fields}->{distribution} eq 'perl' );
+        my $modules;
+        my $data;
+        for (qw(name authorized indexed)) {
+            $data->{$_} = $file->{fields}->{"module.$_"};
+            $data->{$_} = [ $data->{$_} ] unless ( ref $data->{$_} );
+        }
+        for ( my $i = 0; $i < @{ $data->{name} }; $i++ ) {
+            next
+                unless ( $data->{indexed}->[$i] eq "true"
+                && $data->{authorized}->[$i]  eq "true");
+            $result->{ $data->{name}->[$i] } = $file->{fields}->{release};
         }
     }
     return $result;
