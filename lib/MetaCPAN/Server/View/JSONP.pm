08f8a08568ef4b1e63c3b4928f23c94584766bcd
Moritz Onken <onken@netcubed.de>
2011-09-11 11:40:01 +0200

    decode_utf8 jsonp response

--- lib/MetaCPAN/Server/View/JSONP.pm
+++ lib/MetaCPAN/Server/View/JSONP.pm
@@ -1,12 +1,13 @@
 package MetaCPAN::Server::View::JSONP;
 use Moose;
+use Encode qw(decode_utf8);
 extends 'Catalyst::View';
 
 sub process {
     my ($self, $c) = @_;
     return 1 unless(my $cb = $c->req->params->{callback});
     $c->res->body(
-        "$cb(" . $c->res->body . ");"   
+        "$cb(" . decode_utf8($c->res->body) . ");"
     );
     return 1;
 }
