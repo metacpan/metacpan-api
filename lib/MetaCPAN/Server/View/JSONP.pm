0fe00db03881456e974dc057b4e02375cb48b3fd
Grant McLean <grant@mclean.net.nz>
2012-01-24 21:20:38 +1300

    Allow non-JSON content (e.g.: POD) to be delivered via JSONP

--- lib/MetaCPAN/Server/View/JSONP.pm
+++ lib/MetaCPAN/Server/View/JSONP.pm
@@ -1,14 +1,20 @@
 package MetaCPAN::Server::View::JSONP;
 use Moose;
 use Encode qw(decode_utf8);
+use JSON::XS qw();
 extends 'Catalyst::View';
 
 sub process {
     my ($self, $c) = @_;
     return 1 unless(my $cb = $c->req->params->{callback});
-    $c->res->body(
-        "$cb(" . decode_utf8($c->res->body) . ");"
-    );
+    my $body = decode_utf8($c->res->body);
+    my $content_type = $c->res->content_type;
+    if($content_type ne 'application/json') {
+        if(my($key) = $content_type =~ m{^text/(.*)$}) {
+            $body = JSON::XS->new->utf8->encode({ $key => $body });
+        }
+    }
+    $c->res->body( "$cb($body);" );
     return 1;
 }
 
