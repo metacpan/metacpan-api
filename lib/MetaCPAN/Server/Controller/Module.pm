26c3be1e7e1f7459ec8c4b8f9cf7b3de16410076
Moritz Onken <onken@netcubed.de>
2012-02-14 16:21:45 +0100

    ->find requires an inflated set of results

--- lib/MetaCPAN/Server/Controller/Module.pm
+++ lib/MetaCPAN/Server/Controller/Module.pm
@@ -10,8 +10,9 @@ sub index : Chained('/') : PathPart('module') : CaptureArgs(0) {
 sub get : Chained('index') : PathPart('') : Args(1) {
     my ( $self, $c, $module ) = @_;
     eval {
+        $module = $c->model('CPAN::File')->find($module) or die;
         $c->stash(
-            $c->model('CPAN::File')->raw->find($module)->{_source} );
+            $module->meta->get_data($module) );
     } or $c->detach('/not_found');
 }
 
