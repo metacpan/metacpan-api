1ebd90a82e329d022971c0d2a0b10de9d1f79ce4
Moritz Onken <onken@netcubed.de>
2012-02-26 09:50:51 +0100

    cleaner

--- lib/MetaCPAN/Server/Controller/Module.pm
+++ lib/MetaCPAN/Server/Controller/Module.pm
@@ -9,11 +9,9 @@ sub index : Chained('/') : PathPart('module') : CaptureArgs(0) {
 
 sub get : Chained('index') : PathPart('') : Args(1) {
     my ( $self, $c, $module ) = @_;
-    eval {
-        $module = $c->model('CPAN::File')->find($module) or die;
-        $c->stash(
-            $module->meta->get_data($module) );
-    } or $c->detach('/not_found');
+    $module = $c->model('CPAN::File')->find($module)
+        or $c->detach( '/not_found', [$@] );
+    $c->stash( $module->meta->get_data($module) );
 }
 
 1;
