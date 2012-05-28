ac2e20840f353015b5d9758357c4ff9bbbb60330
Moritz Onken <onken@netcubed.de>
2012-05-28 19:35:03 +0200

    the default doesn't pass the TC

--- lib/MetaCPAN/Document/Distribution.pm
+++ lib/MetaCPAN/Document/Distribution.pm
@@ -10,15 +10,13 @@ has name => ( is => 'ro', required => 1, id => 1 );
 has bugs => (
     is      => 'rw',
     isa     => ArrayRef[BugSummary],
-    lazy    => 1,
-    default => sub { [] },
     dynamic => 1,
 );
 
 sub add_bugs {
    my ( $self, @add ) = @_;
-    Bugs->assert_valid($_) for(@add);
-    my $bugs = { map { $_->{type} => $_ } @{ $self->bugs }, @add };
+    BugSummary->assert_valid($_) for(@add);
+    my $bugs = { map { $_->{type} => $_ } @{ $self->bugs || [] }, @add };
     $self->bugs( [ values %$bugs ] );
 }
 
