c27d31ff7588da97067724df29462f2aa637b86e
Olaf Alders <olaf@wundersolutions.com>
2012-10-02 00:51:53 -0400

    Fixes initial file download issues for ratings script.
    Fall back to stringification of the file path if the ratings script does
    not already exist.  resolve() returns undef if the file does not already
    exist, but in the case of a missing file, the object will still
    stringify to the correct path. When called without resolve() in the
    past, the latest version of the file would never be downloaded. So, this
    should now cover the first case where the file does not exist as well as
    the recurring case where the file has already been downloaded.

--- lib/MetaCPAN/Script/Ratings.pm
+++ lib/MetaCPAN/Script/Ratings.pm
@@ -18,8 +18,10 @@ sub run {
     my $self = shift;
     my $ua   = LWP::UserAgent->new;
     log_info { "Downloading " . $self->ratings };
-    my $target = $self->home->file(qw( var tmp ratings.csv ));
-    my $md5 = -e $target ? $self->digest($target) : 0;
+    my @path   = qw( var tmp ratings.csv );
+    my $target = $self->home->file( @path )->resolve
+        || $self->home->file( @path );
+    my $md5 = -e $target ? $self->digest( $target ) : 0;
     my $res = $ua->mirror( $self->ratings, $target );
     if ( $md5 eq $self->digest($target) ) {
         log_info {"No changes to ratings.csv"};
