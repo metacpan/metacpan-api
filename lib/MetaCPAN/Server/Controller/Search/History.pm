24d568efe6ba18e653857a3953e9da202eb5251a
Moritz Onken <onken@netcubed.de>
2013-03-25 18:19:46 -0400

    add /search/history/{module,file,documentation}/... endpoint

---
+++ lib/MetaCPAN/Server/Controller/Search/History.pm
@@ -0,0 +1,14 @@
+package MetaCPAN::Server::Controller::Search::History;
+use Moose;
+BEGIN { extends 'MetaCPAN::Server::Controller' }
+with 'MetaCPAN::Server::Role::JSONP';
+
+has '+type' => ( default => 'file' );
+
+sub get : Local : Path('') : Args {
+    my ( $self, $c, @args ) = @_;
+    my $data = $self->model($c)->history(@args)->raw;
+    $c->stash($data->all);
+}
+
+1;
