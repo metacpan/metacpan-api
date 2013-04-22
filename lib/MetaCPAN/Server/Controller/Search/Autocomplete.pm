860bea7b2d75bb3fdf57256b4207b78345faf685
Moritz Onken <onken@netcubed.de>
2013-04-22 18:46:16 -0400

    allow all fields for autocomplete endpoint, refs Trelane

--- lib/MetaCPAN/Server/Controller/Search/Autocomplete.pm
+++ lib/MetaCPAN/Server/Controller/Search/Autocomplete.pm
@@ -7,8 +7,10 @@ has '+type' => ( default => 'file' );
 
 sub get : Local : Path('') : Args(0) {
     my ( $self, $c ) = @_;
-    my $data = $self->model($c)->autocomplete($c->req->param("q"))->raw
-        ->fields( [qw(documentation release author distribution)] );
+    my $model = $self->model($c);
+    $model = $model->fields( [qw(documentation release author distribution)] )
+    	unless $model->fields;
+    my $data = $model->autocomplete($c->req->param("q"))->raw;
     $c->stash($data->all);
 }
 
