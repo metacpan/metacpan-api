62a9f962bc1ca49d4f43589af423557fb619e6f9
Leo Lapworth <leo@cuckoo.org>
2013-04-25 20:56:32 +0100

    Add a script to mirror CPAN for when developing Metacpan (instead of needing rrr tool)

---
+++ bin/mirror_cpan_for_developers.pl
@@ -0,0 +1,11 @@
+# This script is only needed if you are developing metacpan,
+# on the live servers we use File::Rsync::Mirror::Recent 
+# https://github.com/CPAN-API/Metacpan-Puppet/tree/master/modules/rrrclient
+
+use CPAN::Mini;
+ 
+CPAN::Mini->update_mirror(
+  remote => 'http://www.cpan.org/',
+  local  => "/home/metacpan/CPAN",
+  log_level => 'warn',
+);
