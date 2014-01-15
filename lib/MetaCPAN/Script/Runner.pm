b92bc71017795b2caf96037efc848208df5ec368
Olaf Alders <olaf@wundersolutions.com>
2014-01-15 00:07:42 -0500

    Tidies MetaCPAN::Script::Runner.

--- lib/MetaCPAN/Script/Runner.pm
+++ lib/MetaCPAN/Script/Runner.pm
@@ -10,29 +10,33 @@ use Module::Pluggable search_path => ['MetaCPAN::Script'];
 
 sub run {
     my ( $class, @actions ) = @ARGV;
-    my %plugins = map { (my $key = $_) =~ s/^MetaCPAN::Script:://; lc($key) => $_ } plugins;
+    my %plugins
+        = map { ( my $key = $_ ) =~ s/^MetaCPAN::Script:://; lc($key) => $_ }
+        plugins;
     die "Usage: metacpan [command] [args]" unless ($class);
-    Class::MOP::load_class($plugins{$class});
+    Class::MOP::load_class( $plugins{$class} );
 
     my $config = build_config();
-    my $obj = $plugins{$class}->new_with_options($config);
+    my $obj    = $plugins{$class}->new_with_options($config);
     $obj->run;
 }
 
 sub build_config {
-     my $config = Config::JFDI->new( name => "metacpan",
-                         path => "$FindBin::RealBin/../etc"
-      )->get;
-    if($ENV{HARNESS_ACTIVE}) {
+    my $config = Config::JFDI->new(
+        name => "metacpan",
+        path => "$FindBin::RealBin/../etc"
+    )->get;
+    if ( $ENV{HARNESS_ACTIVE} ) {
         my $tconf = Config::JFDI->new(
-                      name => "metacpan",
-                      file => "$FindBin::RealBin/../etc/metacpan_testing.pl"
+            name => "metacpan",
+            file => "$FindBin::RealBin/../etc/metacpan_testing.pl"
         )->get;
         $config = merge $config, $tconf;
-    } elsif ( is_interactive() ) {
+    }
+    elsif ( is_interactive() ) {
         my $iconf = Config::JFDI->new(
-                      name => "metacpan",
-                      file => "$FindBin::RealBin/../etc/metacpan_interactive.pl"
+            name => "metacpan",
+            file => "$FindBin::RealBin/../etc/metacpan_interactive.pl"
         )->get;
         $config = merge $config, $iconf;
     }
