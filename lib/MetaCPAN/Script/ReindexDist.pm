c4bbc590b91bdc3f6546c185e268c60246b67cfa
Randy Stauner <randy@magnificent-tears.com>
2014-05-19 08:18:52 -0700

    Upgrade bash hack to a Script module
    Improve performance by not reloading the runtime for each url.

---
+++ lib/MetaCPAN/Script/ReindexDist.pm
@@ -0,0 +1,98 @@
+package MetaCPAN::Script::ReindexDist;
+
+# ABSTRACT: Reindex all releases of a distribution
+
+use strict;
+use warnings;
+
+use Moose;
+
+with 'MetaCPAN::Role::Common', 'MooseX::Getopt';
+
+has distribution => (
+    is         => 'ro',
+    isa        => 'Str',
+    lazy_build => 1,
+);
+
+sub _build_distribution {
+    my ($self) = @_;
+
+    # First arg (after script name) is distribution name.
+    # Is there a better way to do this?
+    return $self->extra_argv->[1];
+}
+
+has releases => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+);
+
+sub _build_releases {
+    my ($self) = @_;
+    return [
+        $self->index->type('release')->filter(
+            { term => { 'release.distribution' => $self->distribution } }
+            )->fields( [qw( download_url )] )->sort( ['date'] )->size(5000)
+            ->all
+    ];
+}
+
+has sources => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+);
+
+sub _build_sources {
+    my ($self) = @_;
+    return [ map { $_->download_url } @{ $self->releases } ];
+}
+
+sub script {
+    my $self = shift;
+    local @ARGV = @_;
+    MetaCPAN::Script::Runner->run;
+}
+
+sub run {
+    my ($self) = @_;
+    $self->confirm;
+    $self->script(
+        release => qw(--level debug --detect_backpan),
+        @{ $self->sources }
+    );
+    $self->script( latest => $self->distribution );
+}
+
+sub confirm {
+    my ($self) = @_;
+
+    die "No releases found for ${\ $self->distribution }\n"
+        if !@{ $self->releases };
+
+    print "Reindexing ${\ $self->distribution }\n",
+        ( map {"  $_\n"} @{ $self->sources } ),
+        "Continue? (y/n): ";
+
+    my $confirmation = <STDIN>;
+
+    die "Aborted\n"
+        unless $confirmation =~ /^y/i;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+ # bin/metacpan reindexdist Foo-Bar
+
+=head1 DESCRIPTION
+
+Reindex all the releases of a named distribution.
+
+=cut
