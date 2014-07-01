3ed3983eaa2c46ba869982f0ac1157b871749d88
Talina06 <talina.shrotriya@gmail.com>
2014-07-01 13:08:05 -0400

    Stargazers controller file.

---
+++ lib/MetaCPAN/Server/Controller/Stargazer.pm
@@ -0,0 +1,26 @@
+package MetaCPAN::Server::Controller::Stargazer;
+
+use strict;
+use warnings;
+
+use Moose;
+
+BEGIN { extends 'MetaCPAN::Server::Controller' }
+
+with 'MetaCPAN::Server::Role::JSONP';
+
+sub find : Path('') : Args(2) {
+    my ( $self, $c, $user, $module ) = @_;
+    eval {
+        my $stargazer = $self->model($c)->raw->get(
+            {
+                user         => $user,
+                module	     => $module
+            }
+        );
+        $c->stash( $stargazer->{_source} || $stargazer->{fields} );
+    } or $c->detach( '/not_found', [$@] );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
