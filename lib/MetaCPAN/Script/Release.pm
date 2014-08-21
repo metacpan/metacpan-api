f21555fa9cc6594caa13cfa763584bedbd1a6882
Olaf Alders <olaf@wundersolutions.com>
2014-08-21 11:04:27 -0400

    Only add indexed modules to a file document.
    In the case of Package::Stash 0.36 the first-in logic was preferring
    t/lib/Package/Stash.pm over lib/Package/Stash.pm
    
    Fixes #331.

--- lib/MetaCPAN/Script/Release.pm
+++ lib/MetaCPAN/Script/Release.pm
@@ -16,7 +16,7 @@ use File::Temp ();
 use File::stat ();
 use LWP::UserAgent;
 use List::MoreUtils ();
-use List::Util      ();
+use List::Util qw( first );
 use Log::Contextual qw( :log :dlog );
 use MetaCPAN::Document::Author;
 use MetaCPAN::Script::Latest;
@@ -327,12 +327,14 @@ sub import_tarball {
 
     # find modules
     my @modules;
+
     if ( my %provides = %{ $meta->provides } ) {
         while ( my ( $module, $data ) = each %provides ) {
             my $path = $data->{file};
 
             # FIXME: Could this match lib/Foo.pm and eg/lib/Foo.pm?
-            my $file = List::Util::first { $_->path =~ /\Q$path\E$/ } @files;
+            my $file
+                = first { $_->indexed && $_->path =~ /\Q$path\E$/ } @files;
             next unless $file;
             $file->add_module(
                 {
