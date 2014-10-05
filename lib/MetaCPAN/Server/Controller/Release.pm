a97a9aedb3a94462bd9d5a22d961f1254428f3b7
andreea <andreea.pirvulescu91@gmail.com>
2014-10-05 16:04:56 +0300

    Error response when fields could not be found

--- lib/MetaCPAN/Server/Controller/Release.pm
+++ lib/MetaCPAN/Server/Controller/Release.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Moose;
+use Try::Tiny;
 
 BEGIN { extends 'MetaCPAN::Server::Controller' }
 
@@ -20,23 +21,29 @@ __PACKAGE__->config(
 
 sub find : Path('') : Args(1) {
     my ( $self, $c, $name ) = @_;
-    eval {
-        my $file = $self->model($c)->raw->find($name);
-        $c->stash( $file->{_source} || $file->{fields} );
-    } or $c->detach( '/not_found', [$@] );
+    my $file = $self->model($c)->raw->find($name);
+    if ( !defined $file ) {
+        $c->detach( '/not_found', [] );
+    }
+    try { $c->stash( $file->{_source} || $file->{fields} ) }
+        or $c->detach( '/not_found',
+        ['The requested field(s) could not be found'] );
 }
 
 sub get : Path('') : Args(2) {
     my ( $self, $c, $author, $name ) = @_;
-    eval {
-        my $file = $self->model($c)->raw->get(
-            {
-                author => $author,
-                name   => $name,
-            }
-        );
-        $c->stash( $file->{_source} || $file->{fields} );
-    } or $c->detach( '/not_found', [$@] );
+    my $file = $self->model($c)->raw->get(
+        {
+            author => $author,
+            name   => $name,
+        }
+    );
+    if ( !defined $file ) {
+        $c->detach( '/not_found', [] );
+    }
+    try { $c->stash( $file->{_source} || $file->{fields} ) }
+        or $c->detach( '/not_found',
+        ['The requested field(s) could not be found'] );
 }
 
 1;
