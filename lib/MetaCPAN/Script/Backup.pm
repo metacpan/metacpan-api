16e1792e5abdb802d8082bbc4d9d3bb66e6d2b2c
Olaf Alders <olaf@wundersolutions.com>
2014-10-10 21:41:15 -0400

    Formatting changes in Backup.pm

--- lib/MetaCPAN/Script/Backup.pm
+++ lib/MetaCPAN/Script/Backup.pm
@@ -47,17 +47,22 @@ has restore => (
 
 sub run {
     my $self = shift;
-    return $self->run_purge   if ( $self->purge );
-    return $self->run_restore if ( $self->restore );
+
+    return $self->run_purge   if $self->purge;
+    return $self->run_restore if $self->restore;
+
     my $es = $self->es;
     $self->index->refresh;
-    my $filename = join( "-",
-        DateTime->now->strftime("%F"),
+
+    my $filename = join( '-',
+        DateTime->now->strftime('%F'),
         grep {defined} $self->index->name,
         $self->type );
+
     my $file = $self->home->subdir(qw(var backup))->file("$filename.json.gz");
     $file->dir->mkpath unless ( -e $file->dir );
-    my $fh = IO::Zlib->new( "$file", "wb4" );
+    my $fh = IO::Zlib->new( "$file", 'wb4' );
+
     my $scroll = $es->scrolled_search(
         index => $self->index->name,
         $self->type ? ( type => $self->type ) : (),
@@ -66,7 +71,8 @@ sub run {
         fields      => [qw(_parent _source)],
         scroll      => '1m',
     );
-    log_info { "Backing up ", $scroll->total, " documents" };
+
+    log_info { 'Backing up ', $scroll->total, ' documents' };
 
     while ( my $result = $scroll->next ) {
         print $fh encode_json($result), $/;
@@ -77,12 +83,15 @@ sub run {
 
 sub run_restore {
     my $self = shift;
-    return log_fatal { $self->restore, " doesn't exist" }
+
+    return log_fatal { $self->restore, q{ doesn't exist} }
     unless ( -e $self->restore );
-    log_info { "Restoring from ", $self->restore };
+    log_info { 'Restoring from ', $self->restore };
+
     my @bulk;
     my $es = $self->es;
     my $fh = IO::Zlib->new( $self->restore->stringify, "rb" );
+
     while ( my $line = $fh->readline ) {
         my $obj    = decode_json($line);
         my $parent = $obj->{fields}->{_parent};
@@ -103,16 +112,17 @@ sub run_restore {
     }
     $es->bulk_index( \@bulk );
     log_info {'done'};
-
 }
 
 sub run_purge {
     my $self = shift;
+
     my $now  = DateTime->now;
     $self->home->subdir(qw(var backup))->recurse(
         callback => sub {
             my $file = shift;
             return if ( $file->is_dir );
+
             my $mtime = DateTime->from_epoch( epoch => $file->stat->mtime );
 
             # keep a daily backup for one week
@@ -124,7 +134,7 @@ sub run_purge {
                 != $mtime->clone->truncate( to => 'day' ) )
             {
                 log_info        {"Removing old backup $file"};
-                return log_info {"Not (dry run)"}
+                return log_info {'Not (dry run)'}
                 if ( $self->dry_run );
                 $file->remove;
             }
