0c6017a8ccc42b7b82516e21be2be77107b883d0
Randy Stauner <randy@magnificent-tears.com>
2014-10-24 21:26:01 -0700

    Add some code comments to the login controller

--- lib/MetaCPAN/Server/Controller/Login.pm
+++ lib/MetaCPAN/Server/Controller/Login.pm
@@ -11,6 +11,9 @@ BEGIN { extends 'Catalyst::Controller' }
 
 sub auto : Private {
     my ( $self, $c ) = @_;
+
+    # Store params in a temporary cookie so we can keep track of them.
+    # This should include `client_id` (metacpan env) and `choice` (provider).
     if ( $c->req->params->{client_id} ) {
         $c->res->cookies->{oauth_tmp} = {
             value   => encode_json( $c->req->parameters ),
@@ -18,6 +21,7 @@ sub auto : Private {
             expires => '+7d'
         };
     }
+
     return 1;
 }
 
@@ -42,12 +46,22 @@ sub update_user {
         $user->put( { refresh => 1 } );
     }
     $c->authenticate( { user => $user } );
+
+    # Find the cookie we set earlier.
     if ( my $cid = $c->req->cookie('oauth_tmp') ) {
+
+        # Expire the cookie (tell the browser to remove it).
         $cid->expires('-1y');
+
+        # Pass the params to the oauth controller so it can use them
+        # to redirect the user to the appropriate place.
+        # NOTE: This controller is `lib/Catalyst/Plugin/OAuth2/Provider.pm`.
         $c->res->redirect(
             $c->uri_for( '/oauth2/authorize', decode_json( $cid->value ) ) );
         $c->res->cookies->{oauth_tmp} = $cid;
     }
+
+    # Without the cookie we don't know where to send them.
     else {
         $c->res->redirect('/user');
     }
