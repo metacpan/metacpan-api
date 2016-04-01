cd9b69fcb307aef4f8f1e717e032bbf2427c9cec
Olaf Alders <olaf@wundersolutions.com>
2016-04-01 00:23:55 -0400

    Documents how to display jobs in queue.

--- lib/MetaCPAN/Queue.pm
+++ lib/MetaCPAN/Queue.pm
@@ -8,6 +8,9 @@ queue.
     # On vagrant VM
     ./bin/run morbo bin/queue.pl
 
+    # Display information on jobs in queue
+    ./bin/run bin/queue.pl minion job
+
 =cut
 
 use Mojo::Base 'Mojolicious';
