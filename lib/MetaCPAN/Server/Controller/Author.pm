ab5fb49da13f6e7821925d4649a2de8270fe526b
Mickey Nasriachi <mickey@cpan.org>
2016-07-08 06:44:54 +0100

    get controller for Author, add release count info

--- lib/MetaCPAN/Server/Controller/Author.pm
+++ lib/MetaCPAN/Server/Controller/Author.pm
@@ -24,4 +24,38 @@ __PACKAGE__->config(
     }
 );
 
+sub get : Path('') : Args(1) {
+    my ( $self, $c, $id ) = @_;
+    my $file = $self->model($c)->raw->get($id);
+    if ( !defined $file ) {
+        $c->detach( '/not_found', ['Not found'] );
+    }
+    my $st = $file->{_source} || $file->{fields};
+    if ( $st and $st->{pauseid} ) {
+        $st->{release_count}
+            = $self->_get_author_release_status_counts( $c, $st->{pauseid} );
+    }
+    $c->stash($st)
+        || $c->detach( '/not_found',
+        ['The requested field(s) could not be found'] );
+}
+
+sub _get_author_release_status_counts {
+    my ( $self, $c, $pauseid ) = @_;
+    my %ret;
+    for my $status (qw< cpan backpan latest >) {
+        $ret{$status} = $c->model('CPAN::Release')->filter(
+            {
+                and => [
+                    { term => { author => $pauseid } },
+                    { term => { status => $status } }
+                ]
+            }
+            )->count
+            || 0;
+    }
+    $ret{'backpan-only'} = delete $ret{'backpan'};
+    return \%ret;
+}
+
 1;
