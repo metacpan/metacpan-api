a0af95f573ef899aa47b858a32e3bb03109cdbe8
Graham Knop <haarg@haarg.org>
2016-07-11 18:02:04 -0400

    refactor pod renderer calls
    Rather than getting a renderer and changing settings on it, pass them
    through using attributes on our renderer class.
    
    This brings the HTML renderer more in line with the others.

--- lib/MetaCPAN/Server/View/Pod.pm
+++ lib/MetaCPAN/Server/View/Pod.pm
@@ -11,8 +11,6 @@ extends 'Catalyst::View';
 sub process {
     my ( $self, $c ) = @_;
 
-    my $renderer = MetaCPAN::Pod::Renderer->new;
-
     my $content = $c->res->body || $c->stash->{source};
     my $link_mappings = $c->stash->{link_mappings};
     $content = eval { join( q{}, $content->getlines ) };
@@ -24,21 +22,25 @@ sub process {
     my $x_codes = $c->req->params->{x_codes};
     $x_codes = $c->config->{pod_html_x_codes} unless defined $x_codes;
 
+    my $renderer = $self->_factory(
+        no_errata_section => !$show_errors,
+        nix_X_codes       => !$x_codes,
+        ( $link_mappings ? ( link_mappings => $link_mappings ) : () ),
+    );
     if ( $accept eq 'text/plain' ) {
-        $body         = $self->_factory->to_text($content);
+        $body         = $renderer->to_text($content);
         $content_type = 'text/plain';
     }
     elsif ( $accept eq 'text/x-pod' ) {
-        $body         = $self->_factory->to_pod($content);
+        $body         = $renderer->to_pod($content);
         $content_type = 'text/plain';
     }
     elsif ( $accept eq 'text/x-markdown' ) {
-        $body         = $self->_factory->to_markdown($content);
+        $body         = $renderer->to_markdown($content);
         $content_type = 'text/plain';
     }
     else {
-        $body = $self->build_pod_html( $content, $show_errors, $x_codes,
-            $link_mappings );
+        $body         = $renderer->to_html($content);
         $content_type = 'text/html';
     }
 
@@ -46,23 +48,9 @@ sub process {
     $c->res->body($body);
 }
 
-sub build_pod_html {
-    my ( $self, $source, $show_errors, $x_codes, $link_mappings ) = @_;
-
-    my $renderer = $self->_factory->html_renderer;
-    $renderer->nix_X_codes( !$x_codes );
-    $renderer->no_errata_section( !$show_errors );
-    $renderer->link_mappings($link_mappings);
-
-    my $html = q{};
-    $renderer->output_string( \$html );
-    $renderer->parse_string_document($source);
-    return $html;
-}
-
 sub _factory {
     my $self = shift;
-    return MetaCPAN::Pod::Renderer->new;
+    return MetaCPAN::Pod::Renderer->new(@_);
 }
 
 1;
