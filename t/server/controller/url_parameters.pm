122110a109b9696a3f288a4f876505206f1e7e47
Mickey Nasriachi <mickey@cpan.org>
2016-09-10 17:30:31 +0100

    add test for the URL parameters: test 'source'

---
+++ t/server/controller/url_parameters.pm
@@ -0,0 +1,52 @@
+use strict;
+use warnings;
+
+use Cpanel::JSON::XS ();
+use HTTP::Request::Common qw( GET );
+use MetaCPAN::Server ();
+use Plack::Test;
+use Test::More;
+use Ref::Util qw( is_arrayref is_hashref );
+
+my $app  = MetaCPAN::Server->new->to_app();
+my $test = Plack::Test->create($app);
+
+subtest "parem 'source'" => sub {
+    my $source = Cpanel::JSON::XS::encode_json {
+        query => {
+            term => { distribution => "Moose" }
+        },
+        aggs => {
+            count => {
+                terms => { field => "distribution" }
+            }
+        },
+        size => 0,
+    };
+
+    # test different types, as the parameter is generic to all
+    for ( [ release => 2 ], [ file => 27 ] ) {
+        my ( $type, $count ) = @$_;
+
+        my $url = "/$type/_search?source=$source";
+
+        subtest "check with '$type' controller" => sub {
+            my $res = $test->request( GET $url );
+            ok( $res, "GET $url" );
+            is( $res->code, 200, "code 200" );
+            is(
+                $res->header('content-type'),
+                'application/json; charset=utf-8',
+                'Content-type'
+            );
+            my $content = Cpanel::JSON::XS::decode_json $res->content;
+            ok( is_hashref($content), 'content is a JSON object' );
+            my $buckets = $content->{aggregations}{count}{buckets};
+            ok( is_arrayref($buckets), 'we have aggregation buckets' );
+            is( @{$buckets},              1,      'one key (Moose)' );
+            is( $buckets->[0]{doc_count}, $count, "record count is $count" );
+        };
+    }
+};
+
+done_testing;
