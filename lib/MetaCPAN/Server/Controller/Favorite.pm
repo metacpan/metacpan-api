58200443eb13b99c257c5c7daffebf18e87cb909
Mickey Nasriachi <mickey@cpan.org>
2016-09-29 12:42:28 +0100

    added API endpoint : /favorite/by_user

--- lib/MetaCPAN/Server/Controller/Favorite.pm
+++ lib/MetaCPAN/Server/Controller/Favorite.pm
@@ -8,6 +8,7 @@ use Moose;
 BEGIN { extends 'MetaCPAN::Server::Controller' }
 
 with 'MetaCPAN::Server::Role::JSONP';
+with 'MetaCPAN::Server::Role::ES::Query';
 
 sub find : Path('') : Args(2) {
     my ( $self, $c, $user, $distribution ) = @_;
@@ -24,6 +25,13 @@ sub find : Path('') : Args(2) {
         );
         $c->stash( $favorite->{_source} || $favorite->{fields} );
     } or $c->detach( '/not_found', [$@] );
+}
+
+# endpoint: /favorite/by_user?user=<csv_user_ids>[&fields=<csv_fields>][&sort=<csv_sort>][&size=N]
+sub by_user : Path('by_user') : Args(0) {
+    my ( $self, $c ) = @_;
+    my @users = split /,/ => $c->req->parameters->{user};
+    $self->es_query_by_key( $c, 'user', \@users );
 }
 
 __PACKAGE__->meta->make_immutable;
