a4f78f11d6eaa14ff4f768ebc2f723da4da88bdf
Olaf Alders <olaf@wundersolutions.com>
2016-11-20 13:24:48 -0600

    Handle all scenarios in config builder.

--- lib/MetaCPAN/Role/HasConfig.pm
+++ lib/MetaCPAN/Role/HasConfig.pm
@@ -2,9 +2,10 @@ package MetaCPAN::Role::HasConfig;
 
 use Moose::Role;
 
+use FindBin qw( $RealBin );
+use Hash::Merge::Simple qw(merge);
+use IO::Interactive qw(is_interactive);
 use MetaCPAN::Types qw(HashRef);
-
-use FindBin;
 
 # Done like this so can be required by a roles
 sub config {
@@ -19,11 +20,28 @@ has _config => (
 );
 
 sub _build_config {
-    my $self = shift;
-    return Config::JFDI->new(
+    my $self   = shift;
+    my $config = Config::JFDI->new(
         name => 'metacpan_server',
-        path => "$FindBin::RealBin/..",
+        path => "$RealBin/..",
     )->get;
+
+    if ( $ENV{HARNESS_ACTIVE} ) {
+        my $tconf = Config::JFDI->new(
+            name => 'metacpan',
+            file => 'etc/metacpan_testing.pl'
+        )->get;
+        return merge( $config, $tconf );
+    }
+
+    if ( is_interactive() ) {
+        my $iconf = Config::JFDI->new(
+            name => 'metacpan',
+            file => 'etc/metacpan_interactive.pl'
+        )->get;
+        return merge( $config, $iconf );
+    }
+    return $config;
 }
 
 1;
