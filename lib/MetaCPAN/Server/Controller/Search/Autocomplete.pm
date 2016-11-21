36bdb1e26a7efcfcedd6d8dd6869cd4016696157
Graham Knop <haarg@haarg.org>
2016-11-21 14:13:17 -0500

    only include listed fields in autocomplete output

--- lib/MetaCPAN/Server/Controller/Search/Autocomplete.pm
+++ lib/MetaCPAN/Server/Controller/Search/Autocomplete.pm
@@ -17,7 +17,8 @@ sub get : Local : Path('') : Args(0) {
     my $model = $self->model($c);
     $model = $model->fields( [qw(documentation release author distribution)] )
         unless $model->fields;
-    my $data = $model->autocomplete( $c->req->param("q") )->raw->all;
+    my $data
+        = $model->autocomplete( $c->req->param("q") )->source(0)->raw->all;
 
     single_valued_arrayref_to_scalar( $_->{fields} )
         for @{ $data->{hits}{hits} };
