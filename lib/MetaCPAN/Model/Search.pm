ffa1df8e43a0fcbd9c907d2a4e3dacadbea775e2
Leo Lapworth <leo@cuckoo.org>
2017-05-14 11:05:28 +0100

    use old query instead

--- lib/MetaCPAN/Model/Search.pm
+++ lib/MetaCPAN/Model/Search.pm
@@ -203,15 +203,20 @@ sub _search_collapsed {
     my $es_query = $self->build_query(
         $search_term,
         {
-            # we will probably never hit that limit, since we are
-            # searching in $page_size=20 distributions max
-            size  => 1000,
+# we will probably never hit that limit, since we are searching in $page_size=20 distributions max
+            size  => 5000,
             query => {
-                bool => {
-                    should => {
-                        terms => {
-                            distribution => \@distributions,
-                        }
+                filtered => {
+                    filter => {
+                        and => [
+                            {
+                                or => [
+                                    map {
+                                        { term => { 'distribution' => $_ } }
+                                    } @distributions
+                                ]
+                            }
+                        ]
                     }
                 }
             }
@@ -259,6 +264,159 @@ sub _collapse_results {
 }
 
 sub build_query {
+    my ( $self, $query, $params ) = @_;
+    $params //= {};
+    ( my $clean = $query ) =~ s/::/ /g;
+
+    my $negative
+        = { term => { 'mime' => { value => 'text/x-script.perl' } } };
+
+    my $positive = {
+        bool => {
+            should => [
+
+                # exact matches result in a huge boost
+                {
+                    term => {
+                        'documentation' => {
+                            value => $query,
+                            boost => 20,
+                        }
+                    }
+                },
+                {
+                    term => {
+                        'module.name' => {
+                            value => $query,
+                            boost => 20,
+                        }
+                    }
+                },
+
+            # take the maximum score from the module name and the abstract/pod
+                {
+                    dis_max => {
+                        queries => [
+                            {
+                                query_string => {
+                                    fields => [
+                                        qw(documentation.analyzed^2 module.name.analyzed^2 distribution.analyzed),
+                                        qw(documentation.camelcase module.name.camelcase distribution.camelcase)
+                                    ],
+                                    query                  => $clean,
+                                    boost                  => 3,
+                                    default_operator       => 'AND',
+                                    allow_leading_wildcard => 0,
+                                    use_dis_max            => 1,
+
+                                }
+                            },
+                            {
+                                query_string => {
+                                    fields =>
+                                        [qw(abstract.analyzed pod.analyzed)],
+                                    query                  => $clean,
+                                    default_operator       => 'AND',
+                                    allow_leading_wildcard => 0,
+                                    use_dis_max            => 1,
+
+                                }
+                            }
+                        ]
+                    }
+                }
+
+            ]
+        }
+    };
+
+    my $search = merge(
+        $params,
+        {
+            query => {
+                filtered => {
+                    query => {
+                        function_score => {
+
+                            # prefer shorter module names
+                            script_score => {
+                                script => {
+                                    lang => 'groovy',
+                                    file => 'prefer_shorter_module_names_400',
+                                },
+                            },
+                            query => {
+                                boosting => {
+                                    negative_boost => 0.5,
+                                    negative       => $negative,
+                                    positive       => $positive
+                                }
+                            }
+                        }
+                    },
+                    filter => {
+                        and => [
+                            $self->_not_rogue,
+                            { term => { status       => 'latest' } },
+                            { term => { 'authorized' => 1 } },
+                            { term => { 'indexed'    => 1 } },
+                            {
+                                or => [
+                                    {
+                                        and => [
+                                            {
+                                                exists => {
+                                                    field => 'module.name'
+                                                }
+                                            },
+                                            {
+                                                term => {
+                                                    'module.indexed' => 1
+                                                }
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        exists => { field => 'documentation' }
+                                    },
+                                ]
+                            }
+                        ]
+                    }
+                }
+            },
+            _source => "module",
+            fields  => [
+                qw(
+                    documentation
+                    author
+                    abstract.analyzed
+                    release
+                    path
+                    status
+                    indexed
+                    authorized
+                    distribution
+                    date
+                    id
+                    pod_lines
+                    )
+            ],
+        }
+    );
+
+    # Ensure our requested fields are unique so that Elasticsearch doesn't
+    # return us the same value multiple times in an unexpected arrayref.
+    $search->{fields} = [ uniq @{ $search->{fields} || [] } ];
+
+    return $search;
+}
+
+# This was an attempt at a totally new query but nees lots of
+# work to make the xt/search_web.t pass
+# May need a different querty for non-collapsed (anything
+# that uses lucen syndax, e.g. module:foo)
+sub build_query_attemt_at_new_query {
     my ( $self, $search_term, $params ) = @_;
     $params //= {};
 
