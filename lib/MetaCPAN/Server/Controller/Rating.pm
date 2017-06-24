adb3a6e2dccaa0c5965f17a48d01acfa5fbb715f
Mickey Nasriachi <mickey@cpan.org>
2017-06-24 22:51:28 +0100

    Added /rating/by_distributions API endpoint
    This endpoint will replace query sending from WEB

--- lib/MetaCPAN/Server/Controller/Rating.pm
+++ lib/MetaCPAN/Server/Controller/Rating.pm
@@ -9,4 +9,16 @@ BEGIN { extends 'MetaCPAN::Server::Controller' }
 
 with 'MetaCPAN::Server::Role::JSONP';
 
+sub by_distributions : Path('by_distributions') : Args(0) {
+    my ( $self, $c ) = @_;
+    my $distributions
+        = $c->req->body_data
+        ? $c->req->body_data->{distribution}
+        : [ $c->req->param('distribution') ];
+    return unless $distributions and @{$distributions};
+    my $data = $self->model($c)->raw->by_distributions($distributions);
+    return unless $data;
+    $c->stash($data);
+}
+
 1;
