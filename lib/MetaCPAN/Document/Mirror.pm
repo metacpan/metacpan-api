2787d3c7db4463d3f0cc82ba4317a11f4af0ed4c
Mickey Nasriachi <mickey@cpan.org>
2017-06-24 22:06:08 +0100

    Added /mirror/search API endpoint
    This endpoint will replace query sending from WEB

--- lib/MetaCPAN/Document/Mirror.pm
+++ lib/MetaCPAN/Document/Mirror.pm
@@ -43,4 +43,81 @@ has [qw(inceptdate reitredate)] => (
 );
 
 __PACKAGE__->meta->make_immutable;
+
+package MetaCPAN::Document::Mirror::Set;
+
+use strict;
+use warnings;
+
+use Moose;
+
+extends 'ElasticSearchX::Model::Document::Set';
+
+sub search {
+    my ( $self, $q ) = @_;
+    my $query = { match_all => {} };
+
+    if ($q) {
+        my @protocols = grep /^ (?: http | ftp | rsync ) $/x, split /\s+/, $q;
+
+        my $query = {
+            bool => {
+                must_not => {
+                    bool => {
+                        should => [
+                            map +{ filter => { missing => { field => $_ } } },
+                            @protocols
+                        ]
+                    }
+                }
+            }
+        };
+    }
+
+    my @sort = ( sort => [qw( continent country )] );
+
+    my $location;
+
+    if ( $q and $q =~ /loc\:([^\s]+)/ ) {
+        $location = [ split( /,/, $1 ) ];
+        if ($location) {
+            @sort = (
+                sort => {
+                    _geo_distance => {
+                        location => [ $location->[1], $location->[0] ],
+                        order    => 'asc',
+                        unit     => 'km'
+                    }
+                }
+            );
+        }
+    }
+
+    my $ret = $self->es->search(
+        index => $self->index->name,
+        type  => 'mirror',
+        body  => {
+            size  => 999,
+            query => $query,
+            @sort,
+        },
+    );
+    return unless $ret->{hits}{total};
+
+    my $data = [
+        map +{
+            %{ $_->{_source} },
+            distance => ( $location ? $_->{sort}[0] : undef )
+        },
+        @{ $ret->{hits}{hits} }
+    ];
+
+    return {
+        mirrors => $data,
+        total   => $ret->{hits}{total},
+        took    => $ret->{took}
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
 1;
