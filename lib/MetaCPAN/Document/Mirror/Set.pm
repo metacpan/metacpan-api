bc568793797fd07ba2d16ebb71e46dcd3c552bb8
Mickey Nasriachi <mickey@cpan.org>
2017-11-17 18:28:48 +0000

    Moved Mirror querying to MetaCPAN::Query::Mirror

--- lib/MetaCPAN/Document/Mirror/Set.pm
+++ lib/MetaCPAN/Document/Mirror/Set.pm
@@ -1,76 +1,25 @@
 package MetaCPAN::Document::Mirror::Set;
 
-use strict;
-use warnings;
-
 use Moose;
+
+use MetaCPAN::Query::Mirror;
 
 extends 'ElasticSearchX::Model::Document::Set';
 
-sub search {
-    my ( $self, $q ) = @_;
-    my $query = { match_all => {} };
+has query_mirror => (
+    is      => 'ro',
+    isa     => 'MetaCPAN::Query::Mirror',
+    lazy    => 1,
+    builder => '_build_query_mirror',
+    handles => [qw< search >],
+);
 
-    if ($q) {
-        my @protocols = grep /^ (?: http | ftp | rsync ) $/x, split /\s+/, $q;
-
-        my $query = {
-            bool => {
-                must_not => {
-                    bool => {
-                        should => [
-                            map +{ filter => { missing => { field => $_ } } },
-                            @protocols
-                        ]
-                    }
-                }
-            }
-        };
-    }
-
-    my @sort = ( sort => [qw( continent country )] );
-
-    my $location;
-
-    if ( $q and $q =~ /loc\:([^\s]+)/ ) {
-        $location = [ split( /,/, $1 ) ];
-        if ($location) {
-            @sort = (
-                sort => {
-                    _geo_distance => {
-                        location => [ $location->[1], $location->[0] ],
-                        order    => 'asc',
-                        unit     => 'km'
-                    }
-                }
-            );
-        }
-    }
-
-    my $ret = $self->es->search(
-        index => $self->index->name,
-        type  => 'mirror',
-        body  => {
-            size  => 999,
-            query => $query,
-            @sort,
-        },
+sub _build_query_mirror {
+    my $self = shift;
+    return MetaCPAN::Query::Mirror->new(
+        es         => $self->es,
+        index_name => $self->index->name,
     );
-    return unless $ret->{hits}{total};
-
-    my $data = [
-        map +{
-            %{ $_->{_source} },
-            distance => ( $location ? $_->{sort}[0] : undef )
-        },
-        @{ $ret->{hits}{hits} }
-    ];
-
-    return {
-        mirrors => $data,
-        total   => $ret->{hits}{total},
-        took    => $ret->{took}
-    };
 }
 
 __PACKAGE__->meta->make_immutable;
