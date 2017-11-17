b89f2b72734a78344e5a64e0f49b727b10accbbf
Mickey Nasriachi <mickey@cpan.org>
2017-11-17 16:39:52 +0000

    Moved Author querying to MetaCPAN::Query::Author

--- lib/MetaCPAN/Document/Author/Set.pm
+++ lib/MetaCPAN/Document/Author/Set.pm
@@ -1,111 +1,25 @@
 package MetaCPAN::Document::Author::Set;
 
-use strict;
-use warnings;
-
 use Moose;
+
+use MetaCPAN::Query::Author;
+
 extends 'ElasticSearchX::Model::Document::Set';
 
-use Ref::Util qw( is_arrayref );
+has query_author => (
+    is      => 'ro',
+    isa     => 'MetaCPAN::Query::Author',
+    lazy    => 1,
+    builder => '_build_query_author',
+    handles => [qw< by_ids by_user search >],
+);
 
-use MetaCPAN::Util qw( single_valued_arrayref_to_scalar );
-
-sub by_ids {
-    my ( $self, $ids ) = @_;
-
-    map {uc} @{$ids};
-
-    my $body = {
-        query => {
-            constant_score => {
-                filter => { ids => { values => $ids } }
-            }
-        },
-        size => scalar @{$ids},
-    };
-
-    my $authors = $self->es->search(
-        index => $self->index->name,
-        type  => 'author',
-        body  => $body,
+sub _build_query_author {
+    my $self = shift;
+    return MetaCPAN::Query::Author->new(
+        es         => $self->es,
+        index_name => $self->index->name,
     );
-    return {} unless $authors->{hits}{total};
-
-    my @authors = map {
-        single_valued_arrayref_to_scalar( $_->{_source} );
-        $_->{_source}
-    } @{ $authors->{hits}{hits} };
-
-    return { authors => \@authors };
-}
-
-sub by_user {
-    my ( $self, $users ) = @_;
-    $users = [$users] unless is_arrayref($users);
-
-    my $authors = $self->es->search(
-        index => $self->index->name,
-        type  => 'author',
-        body  => {
-            query => { terms => { user => $users } },
-            size  => 100,
-        }
-    );
-    return {} unless $authors->{hits}{total};
-
-    my @authors = map {
-        single_valued_arrayref_to_scalar( $_->{_source} );
-        $_->{_source}
-    } @{ $authors->{hits}{hits} };
-
-    return { authors => \@authors };
-}
-
-sub search {
-    my ( $self, $query, $from ) = @_;
-
-    my $body = {
-        query => {
-            bool => {
-                should => [
-                    {
-                        match => {
-                            'name.analyzed' =>
-                                { query => $query, operator => 'and' }
-                        }
-                    },
-                    {
-                        match => {
-                            'asciiname.analyzed' =>
-                                { query => $query, operator => 'and' }
-                        }
-                    },
-                    { match => { 'pauseid'    => uc($query) } },
-                    { match => { 'profile.id' => lc($query) } },
-                ]
-            }
-        },
-        size => 10,
-        from => $from || 0,
-    };
-
-    my $ret = $self->es->search(
-        index => $self->index->name,
-        type  => 'author',
-        body  => $body,
-    );
-    return {} unless $ret->{hits}{total};
-
-    my @authors = map {
-        single_valued_arrayref_to_scalar( $_->{_source} );
-        +{ %{ $_->{_source} }, id => $_->{_id} }
-    } @{ $ret->{hits}{hits} };
-
-    return +{
-        authors => \@authors,
-        took    => $ret->{took},
-        total   => $ret->{hits}{total},
-    };
 }
 
 __PACKAGE__->meta->make_immutable;
