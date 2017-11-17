bf3d73f1f8b2156e50a865c7b0b312a252ca968b
Mickey Nasriachi <mickey@cpan.org>
2017-11-17 18:50:13 +0000

    Moved Permission querying to MetaCPAN::Query::Permission

---
+++ lib/MetaCPAN/Query/Permission.pm
@@ -0,0 +1,71 @@
+package MetaCPAN::Query::Permission;
+
+use Moose;
+
+use Ref::Util qw( is_arrayref );
+
+use MetaCPAN::Util qw( single_valued_arrayref_to_scalar );
+
+with 'MetaCPAN::Query::Role::Common';
+
+sub by_author {
+    my ( $self, $pauseid ) = @_;
+
+    my $body = {
+        query => {
+            bool => {
+                should => [
+                    { term => { owner          => $pauseid } },
+                    { term => { co_maintainers => $pauseid } },
+                ],
+            },
+        },
+        size => 5_000,
+    };
+
+    my $ret = $self->es->search(
+        index => $self->index_name,
+        type  => 'permission',
+        body  => $body,
+    );
+    return unless $ret->{hits}{total};
+
+    my $data = [
+        sort { $a->{module_name} cmp $b->{module_name} }
+        map  { $_->{_source} } @{ $ret->{hits}{hits} }
+    ];
+
+    return { permissions => $data };
+}
+
+sub by_modules {
+    my ( $self, $modules ) = @_;
+    $modules = [$modules] unless is_arrayref($modules);
+
+    my @modules = map +{ term => { module_name => $_ } }, @{$modules};
+
+    my $body = {
+        query => {
+            bool => { should => \@modules }
+        },
+        size => 1_000,
+    };
+
+    my $ret = $self->es->search(
+        index => $self->index_name,
+        type  => 'permission',
+        body  => $body,
+    );
+    return unless $ret->{hits}{total};
+
+    my $data = [
+        sort { $a->{module_name} cmp $b->{module_name} }
+        map  { $_->{_source} } @{ $ret->{hits}{hits} }
+    ];
+
+    return { permissions => $data };
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
