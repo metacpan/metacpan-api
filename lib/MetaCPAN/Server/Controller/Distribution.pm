006a32863d0b553a251dc01a2003badb45f2d8db
Mickey Nasriachi <mickey@cpan.org>
2018-04-21 15:05:38 +0100

    Added river data per distribution(s) endpoints

--- lib/MetaCPAN/Server/Controller/Distribution.pm
+++ lib/MetaCPAN/Server/Controller/Distribution.pm
@@ -10,5 +10,16 @@ BEGIN { extends 'MetaCPAN::Server::Controller' }
 
 with 'MetaCPAN::Server::Role::JSONP';
 
+sub river_data_by_dist : Path('river') : Args(1) {
+    my ( $self, $c, $dist ) = @_;
+    $c->stash_or_detach( $self->model($c)->get_river_data_by_dist($dist) );
+}
+
+sub river_data_by_dists : Path('river') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash_or_detach( $self->model($c)
+            ->get_river_data_by_dists( $c->read_param('distribution') ) );
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
