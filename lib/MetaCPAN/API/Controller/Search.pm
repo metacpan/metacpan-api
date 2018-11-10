ca53a54e19a76b7a824bbe744ee6ea2dff4e21fe
Joel Berger <joel.a.berger@gmail.com>
2018-11-10 17:24:55 +0000

    attach search/first to the mojo router

--- lib/MetaCPAN/API/Controller/Search.pm
+++ lib/MetaCPAN/API/Controller/Search.pm
@@ -2,6 +2,18 @@ package MetaCPAN::API::Controller::Search;
 
 use Mojo::Base 'Mojolicious::Controller';
 
+has model => sub { shift->app->model_search };
+
+sub first {
+    my $c = shift;
+    return unless $c->openapi->valid_input;
+    my $args = $c->validation->output;
+
+    my $results = $c->model->search_for_first_result( $args->{q} );
+    return $c->render( openapi => $results ) if $results;
+    $c->rendered(404);
+}
+
 sub web {
     my $c = shift;
     return unless $c->openapi->valid_input;
@@ -9,15 +21,9 @@ sub web {
 
     my @search = ( @{$args}{qw/q from size/} );
     push @search, $args->{collapsed} if exists $args->{collapsed};
-    my $results = $c->app->model_search->search_web(@search);
+    my $results = $c->model->search_web(@search);
 
-    #TODO once output validation works, use this line instead of the one after
-    #return $c->render(openapi => $results);
     return $c->render( json => $results );
-}
-
-sub first {
-
 }
 
 1;
