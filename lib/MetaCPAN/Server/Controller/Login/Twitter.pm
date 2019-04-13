fabe93a832bb8297b26fe15afe047a6b09483a4d
Graham Knop <haarg@haarg.org>
2019-04-13 23:20:20 +0200

    Net::Twitter -> Twitter::API

--- lib/MetaCPAN/Server/Controller/Login/Twitter.pm
+++ lib/MetaCPAN/Server/Controller/Login/Twitter.pm
@@ -7,7 +7,7 @@ use HTTP::Request::Common;
 use Cpanel::JSON::XS;
 use LWP::UserAgent;
 use Moose;
-use Net::Twitter;
+use Twitter::API;
 
 BEGIN { extends 'MetaCPAN::Server::Controller::Login' }
 
@@ -16,10 +16,9 @@ has [qw(consumer_key consumer_secret)] => (
     required => 1,
 );
 
-sub nt {
+sub twitter_api {
     my $self = shift;
-    Net::Twitter->new(
-        traits          => [ 'API::REST', 'OAuth' ],
+    Twitter::API->new(
         consumer_key    => $self->consumer_key,
         consumer_secret => $self->consumer_secret,
     );
@@ -29,13 +28,13 @@ sub index : Path {
     my ( $self, $c ) = @_;
     my $req = $c->req;
     if ( my $code = $req->parameters->{oauth_verifier} ) {
-        my $nt = $self->nt;
-        $nt->request_token( $c->req->cookies->{twitter_token}->value );
-        $nt->request_token_secret(
+        my $api = $self->twitter_api;
+        $api->request_token( $c->req->cookies->{twitter_token}->value );
+        $api->request_token_secret(
             $c->req->cookies->{twitter_token_secret}->value );
 
         my ( $access_token, $access_token_secret, $user_id, $screen_name )
-            = $nt->request_access_token( verifier => $code );
+            = $api->request_access_token( verifier => $code );
         $c->controller('OAuth2')->redirect( $c, error => 'token' )
             unless ($access_token);
         $self->update_user(
@@ -53,15 +52,18 @@ sub index : Path {
         $c->controller('OAuth2')->redirect( $c, error => 'denied' );
     }
     else {
-        my $nt  = $self->nt;
-        my $url = $nt->get_authorization_url(
+        my $api  = $self->twitter_api;
+        my $token = $api->oauth_request_token(
             callback => $c->uri_for( $self->action_for('index') ) );
+        my $url = $api->oauth_authentication_url(
+        );
+        get_authorization_url(
         my $res = $c->res;
         $res->redirect($url);
         $res->cookies->{twitter_token}
-            = { path => '/', value => $nt->request_token };
+            = { path => '/', value => $api->request_token };
         $res->cookies->{twitter_token_secret}
-            = { path => '/', value => $nt->request_token_secret };
+            = { path => '/', value => $api->request_token_secret };
     }
 }
 
