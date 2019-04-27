398a715b2a33b7993aa8e204c79428bc3589c256
Olaf Alders <olaf@wundersolutions.com>
2019-04-27 13:32:49 +0100

    Tidy constants

--- lib/MetaCPAN/Model/Search.pm
+++ lib/MetaCPAN/Model/Search.pm
@@ -25,9 +25,17 @@ has index => (
     required => 1,
 );
 
-const my $RESULTS_PER_RUN => 200;
-const my @ROGUE_DISTRIBUTIONS =>
-    qw(kurila perl_debug perl_mlb perl-5.005_02+apache1.3.3+modperl pod2texi perlbench spodcxx Bundle-Everything);
+const my $RESULTS_PER_RUN     => 200;
+const my @ROGUE_DISTRIBUTIONS => qw(
+    kurila
+    perl_debug
+    perl_mlb
+    perl-5.005_02+apache1.3.3+modperl
+    pod2texi
+    perlbench
+    spodcxx
+    Bundle-Everything
+);
 
 sub _not_rogue {
     my @rogue_dists
