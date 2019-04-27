7a2a12d36ac76f613678cf13ba99b7c4740237aa
Olaf Alders <olaf@wundersolutions.com>
2019-04-27 10:46:21 +0100

    Sort use statements in MetaCPAN::Server

--- lib/MetaCPAN/Server.pm
+++ lib/MetaCPAN/Server.pm
@@ -4,15 +4,14 @@ use Moose;
 
 ## no critic (Modules::RequireEndWithOne)
 use Catalyst qw( +MetaCPAN::Role::Fastly::Catalyst ), '-Log=warn,error,fatal';
-use Log::Log4perl::Catalyst;
-
 use CatalystX::RoleApplicator;
+use Digest::SHA;
 use File::Temp qw( tempdir );
+use Log::Log4perl::Catalyst;
+use Plack::Builder;
 use Plack::Middleware::ReverseProxy;
 use Plack::Middleware::ServerStatus::Lite;
 use Ref::Util qw( is_arrayref is_hashref );
-use Plack::Builder;
-use Digest::SHA;
 
 extends 'Catalyst';
 
