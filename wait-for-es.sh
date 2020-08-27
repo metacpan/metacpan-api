025c1b4c213bff0c11ff1dcdd0a851cbd12be4ab
Olaf Alders <olaf@wundersolutions.com>
2020-08-26 20:39:11 -0400

    Simplify wait-for-es.sh script

--- wait-for-es.sh
+++ wait-for-es.sh
@@ -3,40 +3,36 @@
 # Courtesy of @fxdgear
 # https://github.com/elastic/elasticsearch-py/issues/778#issuecomment-384389668
 
-set -e
+set -ux
 
 host="$1"
-shift
-cmd="$@"
 
+while true; do
+    response=$(curl --write-out '%{http_code}' --silent --fail --output /dev/null "$host")
+    if [[ "$response" -eq "200" ]]; then
+        break
+    fi
 
-until $(curl --output /dev/null --silent --head --fail "$host"); do
-    printf '.'
+    echo "Elastic Search is unavailable - sleeping" >&2
     sleep 1
 done
 
-# First wait for ES to start...
-response=$(curl $host)
+# set -e now because it was causing the curl command above to exit the script
+# if the server was not available
+set -e
 
-until [ "$response" = "200" ]; do
-    response=$(curl --write-out %{http_code} --silent --output /dev/null "$host")
-    >&2 echo "Elastic Search is unavailable - sleeping"
-    sleep 1
-done
-
-
-# Wait for ES status to turn to yellow.
-# TODO: Ideally we'd be waiting for green, but we need multiple nodes for that.
-
-health="$(curl -fsSL "$host/_cat/health?h=status")"
-health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
-
-until [ "$health" = 'yellow' ]; do
+while true; do
+    ## Wait for ES status to turn to yellow.
+    ## TODO: Ideally we'd be waiting for green, but we need multiple nodes for that.
     health="$(curl -fsSL "$host/_cat/health?h=status")"
-    health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
-    >&2 echo "Elastic Search is unavailable ($health) - sleeping"
+    health="$(echo "$health" | xargs)" # trim whitespace (otherwise we'll have "green ")
+    if [[ $health == 'yellow' || $health == 'green' ]]; then
+        break
+    fi
+    echo "Elastic Search is unavailable ($health) - sleeping" >&2
     sleep 1
 done
 
->&2 echo "Elastic Search is up"
-exec $cmd
+echo "Elastic Search is up" >&2
+shift
+exec "$@"
