8399420b0d9d3abbb13a50d93eb9696bff5667d2
Graham Knop <haarg@haarg.org>
2021-07-16 12:20:42 +0200

    respond to deserialization errors with json
    If there is an error while deserializing, it is nicer to respond with a
    JSON response, rather than plain text. Use a custom Deserialize action
    class, which puts the error into the stash where either the Serialize
    action class or the JSON view expects. And then skip the rest of the
    request.

---
+++ lib/MetaCPAN/Server/Action/Deserialize.pm
@@ -0,0 +1,28 @@
+package MetaCPAN::Server::Action::Deserialize;
+use Moose;
+extends 'Catalyst::Action::Deserialize';
+use Cpanel::JSON::XS qw(encode_json);
+
+around serialize_bad_request => sub {
+    my $orig = shift;
+    my $self = shift;
+    my ( $c, $content_type, $error ) = @_;
+
+    $c->res->status(400);
+
+    my $full_error
+        = "Content-Type $content_type had a problem with your request.\n$error";
+    $full_error =~ s{ at .*? line \d+\.\n\z}{};
+
+    $c->stash(
+        {
+            rest => {
+                error => $full_error,
+            },
+        }
+    );
+
+    return undef;
+};
+
+1;
