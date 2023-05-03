9abc57c1b2373d0c33c91e75651628fbf61c6501
Graham Knop <haarg@haarg.org>
2023-05-03 16:58:27 +0200

    add cve dist end point

--- lib/MetaCPAN/Query/CVE.pm
+++ lib/MetaCPAN/Query/CVE.pm
@@ -38,5 +38,27 @@ sub find_cves_by_release {
     return +{ cve => [ map { $_->{_source} } @{ $res->{hits}{hits} } ] };
 }
 
+sub find_cves_by_dist {
+    my ( $self, $dist, $version ) = @_;
+
+    my $query = +{
+        match => {
+            dist => $dist,
+            ( defined $version ? ( versions => $version ) : () ),
+        }
+    };
+
+    my $res = $self->es->search(
+        index => $self->index_name,
+        type  => 'cve',
+        body  => {
+            query => $query,
+            size  => 999,
+        }
+    );
+
+    return +{ cve => [ map { $_->{_source} } @{ $res->{hits}{hits} } ] };
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
--- lib/MetaCPAN/Server/Controller/CVE.pm
+++ lib/MetaCPAN/Server/Controller/CVE.pm
@@ -21,4 +21,11 @@ sub release : Path('release') : Args(2) {
         $self->model($c)->find_cves_by_release( $author, $release ) );
 }
 
+sub dist : Path('dist') : Args(1) {
+    my ( $self, $c, $dist ) = @_;
+    my $version = $c->req->query_params->{version};
+    $c->stash_or_detach(
+        $self->model($c)->find_cves_by_dist( $dist, $version ) );
+}
+
 1;
