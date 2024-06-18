02f30ead29deb4486a829c0a74215e74e6b5e331
Graham Knop <haarg@haarg.org>
2024-06-18 18:18:50 +0200

    remove unused dep BackPAN::Index

--- lib/MetaCPAN/Script/Backpan.pm
+++ lib/MetaCPAN/Script/Backpan.pm
@@ -5,8 +5,7 @@ use warnings;
 
 use Moose;
 
-use Log::Contextual qw( :log :dlog );
-use BackPAN::Index;
+use Log::Contextual           qw( :log :dlog );
 use MetaCPAN::Types::TypeTiny qw( Bool HashRef Str );
 
 with 'MetaCPAN::Role::Script', 'MooseX::Getopt::Dashes';
