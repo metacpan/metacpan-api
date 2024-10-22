4abdc0041cad3773a3f7b0542e034b4d49041fe7
Graham Knop <haarg@haarg.org>
2024-10-22 21:08:13 +0200

    configure MetaCPAN::Model via ESConfig
    Rather than searching for modules on disk, use the explicit
    configuration in ESConfig to configure MetaCPAN::Model.

--- lib/MetaCPAN/Model.pm
+++ lib/MetaCPAN/Model.pm
@@ -4,14 +4,37 @@ package MetaCPAN::Model;
 use Moose;
 
 use ElasticSearchX::Model;
+use MetaCPAN::ESConfig qw(es_config);
+use Module::Runtime    qw(require_module);
 
-index cpan => (
-    namespace => 'MetaCPAN::Document',
-    alias_for => 'cpan_v1_01',
-    shards    => 3
-);
+my %indexes;
+my $docs = es_config->documents;
+for my $name ( sort keys %$docs ) {
+    my $doc   = $docs->{$name};
+    my $model = $doc->{model}
+        or next;
+    require_module($model);
+    my $index = $doc->{index}
+        or die "no index for $name documents!";
 
-index user => ( namespace => 'MetaCPAN::Model::User' );
+    $indexes{$index}{types}{$name} = $model->meta;
+}
+
+my $aliases = es_config->aliases;
+for my $alias ( sort keys %$aliases ) {
+    my $index      = $aliases->{$alias};
+    my $index_data = $indexes{$index}
+        or die "unknown index $index";
+    if ( $index_data->{alias_for} ) {
+        die "duplicate alias for $index";
+    }
+    $index_data->{alias_for} = $index;
+    $indexes{$alias} = delete $indexes{$index};
+}
+
+for my $index ( sort keys %indexes ) {
+    index $index => %{ $indexes{$index} };
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
