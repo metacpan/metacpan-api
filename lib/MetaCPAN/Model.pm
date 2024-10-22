bb889c950d1e3396f85db522539b998ea5941c44
Graham Knop <haarg@haarg.org>
2024-10-22 21:05:02 +0200

    remove analysis configuration from MetaCPAN::Model
    The analysis set in MetaCPAN::Model wasn't used for anything directly,
    generate the index deployment statements. The index settings we actually
    use lives in MetaCPAN::Script::Mapping::DeployStatement, so the
    declarations in MetaCPAN::Model were used for nothing.

--- lib/MetaCPAN/Model.pm
+++ lib/MetaCPAN/Model.pm
@@ -5,43 +5,6 @@ use Moose;
 
 use ElasticSearchX::Model;
 
-analyzer lowercase => (
-    tokenizer => 'keyword',
-    filter    => 'lowercase',
-);
-
-analyzer fulltext => ( type => 'english' );
-
-tokenizer camelcase => (
-    type    => 'pattern',
-    pattern =>
-        "([^\\p{L}\\d]+)|(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)|(?<=[\\p{L}&&[^\\p{Lu}]])(?=\\p{Lu})|(?<=\\p{Lu})(?=\\p{Lu}[\\p{L}&&[^\\p{Lu}]])"
-);
-
-filter edge => (
-    type     => 'edge_ngram',
-    min_gram => 1,
-    max_gram => 20
-);
-
-analyzer camelcase => (
-    type      => 'custom',
-    tokenizer => 'camelcase',
-    filter    => [ 'lowercase', 'unique' ]
-);
-
-analyzer edge_camelcase => (
-    type      => 'custom',
-    tokenizer => 'camelcase',
-    filter    => [ 'lowercase', 'edge' ]
-);
-
-analyzer edge => (
-    type      => 'custom',
-    tokenizer => 'standard',
-    filter    => [ 'lowercase', 'edge' ]
-);
-
 index cpan => (
     namespace => 'MetaCPAN::Document',
     alias_for => 'cpan_v1_01',
