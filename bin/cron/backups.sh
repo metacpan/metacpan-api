690f419c32d4ba8e650ac4e5a453b2dc2685a934
Graham Knop <haarg@haarg.org>
2026-01-27 10:06:02 +0100

    remove unused cron wrapper scripts

--- bin/cron/backups.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/home/metacpan/bin/metacpan-api-carton-exec bin/metacpan backup --index cpan_v1_01 --type favorite
-/home/metacpan/bin/metacpan-api-carton-exec bin/metacpan backup --index cpan_v1_01 --type author
-
-/home/metacpan/bin/metacpan-api-carton-exec bin/metacpan backup --index user
