f5207e9b7b4f129cb9fffb8629275c278a8cb089
Graham Knop <haarg@haarg.org>
2026-01-27 15:30:40 +0100

    simplify minicpan script

--- bin/mirror_cpan_for_developers.pl
+++ bin/mirror_cpan_for_developers.pl
@@ -1,15 +1,7 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
+#!/bin/sh
 
 # This script is only needed if you are developing metacpan,
 # on the live servers we use File::Rsync::Mirror::Recent
 # https://github.com/metacpan/metacpan-puppet/tree/master/modules/rrrclient
 
-use CPAN::Mini;
-
-CPAN::Mini->update_mirror(
-    remote    => 'http://www.cpan.org/',
-    local     => "/home/metacpan/CPAN",
-    log_level => 'warn',
-);
+minicpan -l /home/metacpan/CPAN -r https://www.cpan.org/ --log-level=warn
